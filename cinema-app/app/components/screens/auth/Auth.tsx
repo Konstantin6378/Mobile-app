50ebc5980bce12af6e75d2ae4478f910fdfcb664
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-04 16:55:56 +0800

    add
    add DismissKyeboard in file Auth

--- cinema-app/app/components/screens/auth/Auth.tsx
+++ cinema-app/app/components/screens/auth/Auth.tsx
@@ -3,6 +3,7 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import { Pressable, Text, View } from 'react-native'
 
 import { Button, Loader } from '@/components/ui'
+import DismissKeyboard from '@/components/ui/form-elements/field/DismissKeyboard'
 
 import { IAuthFormData } from '@/shared/types/auth.interface'
 
@@ -19,29 +20,31 @@ const Auth: FC = () => {
 
 	const isLoading = false
 	return (
-		<View className='mx-2 items-center justify-center'>
-			<View className='w-9/12'>
-				<Text className='text-center text-white text-4xl font-bold mb-2.5'>
-					{isReg ? 'Register' : 'Login'}
-				</Text>
+		<DismissKeyboard>
+			<View className='mx-2 items-center justify-center'>
+				<View className='w-9/12'>
+					<Text className='text-center text-white text-4xl font-bold mb-2.5'>
+						{isReg ? 'Register' : 'Login'}
+					</Text>
 
-				{isLoading ? (
-					<Loader />
-				) : (
-					<>
-						<AuthFields control={control} isPassRequired />
-						<Button onPress={handleSubmit(onSubmit)} icon={'film'}>
-							Go to watch
-						</Button>
-						<Pressable onPress={() => setIsReg(!isReg)}>
-							<Text className='text-white opacity-30 text-right text-base mt-3'>
-								{isReg ? 'Login' : 'Register'}
-							</Text>
-						</Pressable>
-					</>
-				)}
+					{isLoading ? (
+						<Loader />
+					) : (
+						<>
+							<AuthFields control={control} isPassRequired />
+							<Button onPress={handleSubmit(onSubmit)} icon={'film'}>
+								Go to watch
+							</Button>
+							<Pressable onPress={() => setIsReg(!isReg)}>
+								<Text className='text-white opacity-30 text-right text-base mt-3'>
+									{isReg ? 'Login' : 'Register'}
+								</Text>
+							</Pressable>
+						</>
+					)}
+				</View>
 			</View>
-		</View>
+		</DismissKeyboard>
 	)
 }
 export default Auth
