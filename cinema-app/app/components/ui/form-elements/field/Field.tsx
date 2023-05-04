b826e8bb7be84b366802f9daa2abd72fea881d3b
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-04 16:33:36 +0800

    add
    component field

--- cinema-app/app/components/ui/form-elements/field/Field.tsx
+++ cinema-app/app/components/ui/form-elements/field/Field.tsx
@@ -1,11 +1,45 @@
-import { FC } from 'react'
-import { Text, View } from 'react-native'
+import cn from 'clsx'
+import { Controller } from 'react-hook-form'
+import { Text, TextInput, View } from 'react-native'
 
-const Field: FC = () => {
+import { IField } from './field.interface'
+
+const Field = <T extends Record<string, any>>({
+	control,
+	rules,
+	name,
+	className,
+	...rest
+}: IField<T>): JSX.Element => {
 	return (
-		<View>
-			<Text>Field</Text>
-		</View>
+		<Controller
+			control={control}
+			name={name}
+			rules={rules}
+			render={({
+				field: { value, onChange, onBlur },
+				fieldState: { error }
+			}) => (
+				<>
+					<View
+						className={cn(
+							'bg-[#232323] w-full border rounded-lg pb-4 pt-2.5 px-4 my-1.5',
+							error ? 'border-red' : 'border-transparent'
+						)}
+					>
+						<TextInput
+							autoCapitalize={'none'}
+							onChangeText={onChange}
+							onBlur={onBlur}
+							value={(value || '').toString()}
+							className='text-white text-base'
+							{...rest}
+						/>
+					</View>
+					{error && <Text className='text-red'>{error.message}</Text>}
+				</>
+			)}
+		/>
 	)
 }
 export default Field
