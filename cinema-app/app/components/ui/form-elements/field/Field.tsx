af2d531d6a6637fd11f0e90049b6ce2defe74a87
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-04 16:22:15 +0800

    add
    created generic form-element

---
+++ cinema-app/app/components/ui/form-elements/field/Field.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const Field: FC = () => {
+	return (
+		<View>
+			<Text>Field</Text>
+		</View>
+	)
+}
+export default Field
