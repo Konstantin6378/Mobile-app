097f0bf3fbdf7c2e2c0c15db2e1c1a7cfcd7265f
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-04 12:46:16 +0800

    add
    add hook-form create auth interface

--- cinema-app/app/components/screens/auth/Auth.tsx
+++ cinema-app/app/components/screens/auth/Auth.tsx
@@ -1,7 +1,15 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Text, View } from 'react-native'
 
 const Auth: FC = () => {
+	const [isReg, setIsReg] = useState(false)
+
+	const { handleSubmit, reset, control } = useForm({
+		mode: 'onChange'
+	})
+
+	const onSubmit: SubmitHandler<any> = data => {}
 	return (
 		<View>
 			<Text>Auth</Text>
