91c65615106f3f3ce72007aff435ff72c12dca60
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-04 12:50:51 +0800

    add
    add interface Auth

--- cinema-app/app/components/screens/auth/Auth.tsx
+++ cinema-app/app/components/screens/auth/Auth.tsx
@@ -2,14 +2,16 @@ import { FC, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Text, View } from 'react-native'
 
+import { IAuthFormData } from '@/shared/types/auth.interface'
+
 const Auth: FC = () => {
 	const [isReg, setIsReg] = useState(false)
 
-	const { handleSubmit, reset, control } = useForm({
+	const { handleSubmit, reset, control } = useForm<IAuthFormData>({
 		mode: 'onChange'
 	})
 
-	const onSubmit: SubmitHandler<any> = data => {}
+	const onSubmit: SubmitHandler<IAuthFormData> = ({ email, password }) => {}
 	return (
 		<View>
 			<Text>Auth</Text>
