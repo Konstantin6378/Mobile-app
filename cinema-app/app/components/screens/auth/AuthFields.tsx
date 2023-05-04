eb31e923cb4a8adeb9f18625670f57b7e61df216
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-04 16:45:16 +0800

    add
    add in AuthFields validation Email and Password

--- cinema-app/app/components/screens/auth/AuthFields.tsx
+++ cinema-app/app/components/screens/auth/AuthFields.tsx
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native'
 
 import Field from '@/components/ui/form-elements/field/Field'
 
+import { validEmail } from '@/shared/regex'
 import { IAuthFormData } from '@/shared/types/auth.interface'
 
 interface IAuthFields {
@@ -15,11 +16,33 @@ const AuthFields: FC<IAuthFields> = ({ control, isPassRequired }) => {
 	return (
 		<>
 			<Field<IAuthFormData>
+				placeholder='Enter email address'
 				control={control}
 				name='email'
 				rules={{
-					required: 'Email'
+					required: 'Email',
+					pattern: {
+						value: validEmail,
+						message: 'Please enter a valid email address'
+					}
 				}}
+				keyboardType='email-address'
+			/>
+			<Field<IAuthFormData>
+				placeholder='Enter password'
+				control={control}
+				name='password'
+				rules={
+					isPassRequired
+						? {
+								required: 'Password is required',
+								minLength: {
+									value: 6,
+									message: 'Password should be minimum 6 characters long'
+								}
+						  }
+						: {}
+				}
 			/>
 		</>
 	)
