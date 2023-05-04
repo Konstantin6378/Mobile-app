aae723651c9bb3844029075d3d142d0919d8d44f
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-04 13:22:01 +0800

    add
    expo LinearGradient

--- cinema-app/app/components/ui/button/Button.tsx
+++ cinema-app/app/components/ui/button/Button.tsx
@@ -1,14 +1,26 @@
-import { FC, PropsWithChildren } from 'react';
+import cn from 'clsx'
+import { LinearGradient } from 'expo-linear-gradient'
+import { FC, PropsWithChildren } from 'react'
 import { Pressable, Text } from 'react-native'
-import { IButton } from './button.interface';
-import cn from 'clsx';
 
+import { IButton } from './button.interface'
 
-
-const Button: FC<PropsWithChildren<IButton>> = ({className, icon, children,  ...rest}) => {
+const Button: FC<PropsWithChildren<IButton>> = ({
+	className,
+	icon,
+	children,
+	...rest
+}) => {
 	return (
-		<Pressable className={cn('self-center mt-3.5' , className)} onPress={}>
-			<Text>{children}</Text>
+		<Pressable className={cn('self-center mt-3.5', className)}>
+			<LinearGradient
+				className={cn('w-full py-3 px-8 rounded-2xl items-center', {
+					'flex-row': !!icon
+				})}
+				colors={['#DC3F41', '#A6282B']}
+			>
+				<Text>{children}</Text>
+			</LinearGradient>
 		</Pressable>
 	)
 }
