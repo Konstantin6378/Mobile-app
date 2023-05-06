bf1ce0909dba89ba97edd8b4e0593a2f31970557
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-06 16:18:39 +0800

    add
    add State and Effect in AuthProvider

--- cinema-app/app/components/providers/auth/AuthProvider.tsx
+++ cinema-app/app/components/providers/auth/AuthProvider.tsx
@@ -1,7 +1,21 @@
-import { FC, PropsWithChildren } from 'react'
+import {
+	FC,
+	PropsWithChildren,
+	createContext,
+	useEffect,
+	useState
+} from 'react'
 import { View } from 'react-native'
 
+import { IContext, TypeUserState } from './auth-provider.interface'
+
+export const AuthContext = createContext({} as IContext)
+
 const AuthProvider: FC<PropsWithChildren<unknown>> = ({ children }) => {
+	const [user, setUser] = useState<TypeUserState>(null)
+
+	useEffect(() => {}, [])
+
 	return <View>{children}</View>
 }
 export default AuthProvider
