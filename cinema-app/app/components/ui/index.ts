27fc2d4da5e975a398aae3d95f594c6ada8bd85b
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-07 15:16:13 +0800

    add
    add structure bottom-menu

--- cinema-app/app/components/ui/index.ts
+++ cinema-app/app/components/ui/index.ts
@@ -3,3 +3,10 @@ export { default as Button } from './button/Button'
 
 export { default as DismissKeyboard } from './form-elements/field/DismissKeyboard'
 export { default as Field } from './form-elements/field/Field'
+
+export { default as Auth } from '../screens/auth/Auth'
+export { default as Favorites } from '../screens/favorites/Favorites'
+export { default as Home } from '../screens/home/Home'
+export { default as Profile } from '../screens/profile/Profile'
+export { default as Search } from '../screens/search/Search'
+export { default as Trending } from '../screens/trending/Trending'
