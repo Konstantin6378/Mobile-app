cc68bde8b7b7afb7a41d7f8e04f6f288a86e2306
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-07 15:37:03 +0800

    update
    Navigate

--- cinema-app/app/navigation/Navigation.tsx
+++ cinema-app/app/navigation/Navigation.tsx
@@ -1,14 +1,29 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { FC } from 'react'
+import {
+	NavigationContainer,
+	useNavigationContainerRef
+} from '@react-navigation/native'
+import { FC, useState } from 'react'
+
+import BottomMenu from '@/components/ui/layout/bottom-menu/BottomMenu'
+
+import { useAuth } from '@/hooks/useAuth'
 
 import PrivateNavigator from './PrivateNavigator'
 
 const Navigation: FC = () => {
+	const { user } = useAuth()
+	const [currentRoute, setCurrentRoute] = useState(undefined)
+
+	const navRef = useNavigationContainerRef()
 	return (
-		<NavigationContainer>
-			<PrivateNavigator />
-		</NavigationContainer>
-		// TODO: Bottom menu
+		<>
+			<NavigationContainer>
+				<PrivateNavigator />
+			</NavigationContainer>
+			{user && currentRoute && (
+				<BottomMenu nav={navRef.navigate} currentRoute={currentRoute} />
+			)}
+		</>
 	)
 }
 export default Navigation
