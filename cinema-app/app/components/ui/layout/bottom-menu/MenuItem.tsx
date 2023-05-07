996b242deb51fdd180681e46625b729af6f304ff
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-07 15:25:41 +0800

    create
    create menuItem

--- cinema-app/app/components/ui/layout/bottom-menu/MenuItem.tsx
+++ cinema-app/app/components/ui/layout/bottom-menu/MenuItem.tsx
@@ -1,24 +1,27 @@
-import { FC } from 'react';
-import { Pressable, Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+import { FC } from 'react'
+import { Pressable, Text, View } from 'react-native'
 
+import { getColor } from '@/config/colors.config'
 
-
-import { IMenuItem } from './menu.interface';
-import { TypeRootStackParamList } from '@/navigation/navigation.types';
-import { Feather } from '@expo/vector-icons';
-
+import { IMenuItem } from './menu.interface'
+import { TypeRootStackParamList } from '@/navigation/navigation.types'
 
 interface IMenuItemProps {
 	item: IMenuItem
-  nav: (screenName: keyof TypeRootStackParamList) => void
-  currentRoute?: string
+	nav: (screenName: keyof TypeRootStackParamList) => void
+	currentRoute?: string
 }
 
 const MenuItem: FC<IMenuItemProps> = ({ item, nav, currentRoute }) => {
-  const isActive = currentRoute === item.path
+	const isActive = currentRoute === item.path
 	return (
 		<Pressable className='items-center w-[20%]' onPress={() => nav(item.path)}>
-			<Feather name={item.iconName} size={26} color={isActive ? } />
+			<Feather
+				name={item.iconName}
+				size={26}
+				color={isActive ? getColor('primary') : getColor('gray-400')}
+			/>
 		</Pressable>
 	)
 }
