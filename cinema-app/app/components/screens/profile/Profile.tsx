6ee7bf11934c3755d851a89037b163251d715f0f
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-08 20:26:43 +0800

    Update Profile.tsx

--- cinema-app/app/components/screens/profile/Profile.tsx
+++ cinema-app/app/components/screens/profile/Profile.tsx
@@ -1,10 +1,22 @@
+import { AntDesign } from '@expo/vector-icons'
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
+
+import { useAuth } from '@/hooks/useAuth'
+
+import { AuthService } from '@/services/auth/auth.service'
 
 const Profile: FC = () => {
+	const { setUser } = useAuth()
 	return (
-		<View>
-			<Text>Profile</Text>
+		<View className='mt-20 px-10'>
+			<Pressable
+				onPress={() => AuthService.logout().then(() => setUser(null))}
+				className='opacity-40 items-center flex-row justify-end'
+			>
+				<AntDesign name={'logout'} size={18} color='white' />
+				<Text className='text-white text-lg ml-2'>Logout</Text>
+			</Pressable>
 		</View>
 	)
 }
