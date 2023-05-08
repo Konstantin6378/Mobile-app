e1455b749ae37e8aa2a2e740fd06d32226fa72ad
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-08 18:03:01 +0800

    Update interseptors.api.ts

--- cinema-app/app/services/api/interseptors.api.ts
+++ cinema-app/app/services/api/interseptors.api.ts
@@ -1,13 +1,30 @@
-import { API_URL } from "@/config/api.config";
-import axios from "axios";
+import axios from 'axios'
+
+import { API_URL } from '@/config/api.config'
+
+import { getAccessToken } from '../auth/auth.helper'
 
 const instance = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json'
-  }
+	baseURL: API_URL,
+	headers: {
+		'Content-Type': 'application/json'
+	}
 })
 
-instance.interceptors.request.use(async () => {
-  const accessToken = await
+instance.interceptors.request.use(async config => {
+	const accessToken = await getAccessToken()
+
+	if (config.headers && accessToken)
+		config.headers.Authorization = `Bearer ${accessToken}`
+
+	return config
+})
+
+instance.interceptors.request.use(async config => {
+	const accessToken = await getAccessToken()
+
+	if (config.headers && accessToken)
+		config.headers.Authorization = `Bearer ${accessToken}`
+
+	return config
 })
