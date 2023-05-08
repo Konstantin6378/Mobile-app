253546a14f587747061f9c1b0b541a883807e6f1
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-08 17:41:06 +0800

    Update auth.interface.ts
    enum tokens

--- cinema-app/app/shared/types/auth.interface.ts
+++ cinema-app/app/shared/types/auth.interface.ts
@@ -1,3 +1,17 @@
 import { IUser } from './user.interface'
 
 export interface IAuthFormData extends Pick<IUser, 'email' | 'password'> {}
+
+export enum EnumSecureStore {
+	ACCESS_TOKEN = 'accessToken',
+	REFRESH_TOKEN = 'refreshToken'
+}
+
+export interface ITokens {
+	accessToken: string
+	refreshToken: string
+}
+
+export interface IAuthResponse extends ITokens {
+	user: IUser
+}
