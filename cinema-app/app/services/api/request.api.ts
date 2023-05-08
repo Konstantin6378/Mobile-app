1a582f64da02e2ddea14ba8e63c57832356de64c
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-08 17:20:35 +0800

    created
    request.api

--- cinema-app/app/services/api/request.api.ts
+++ cinema-app/app/services/api/request.api.ts
@@ -1,1 +1,17 @@
-export const request = 
+import { AxiosError, AxiosResponse } from 'axios'
+import { Toast } from 'react-native-toast-message/lib/src/Toast'
+
+import { errorCath } from './error.api'
+
+export const request = async <T>() => {
+	const onSuccess = (response: AxiosResponse<T>) => response.data
+	const onError = (error: AxiosError<T>) => {
+		Toast.show({
+			type: 'error',
+			text1: 'Request error',
+			text2: errorCath(error)
+		})
+
+		return Promise.reject(error)
+	}
+}
