cceff22dde42b131d46b579d96259842d1df06f1
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-08 19:27:47 +0800

    Update useCheckAuth.ts

--- cinema-app/app/components/providers/auth/useCheckAuth.ts
+++ cinema-app/app/components/providers/auth/useCheckAuth.ts
@@ -1,1 +1,41 @@
-export const useCheckAuth = () => {}
+import { getItemAsync } from 'expo-secure-store'
+import { useEffect } from 'react'
+
+import { useAuth } from '@/hooks/useAuth'
+
+import { EnumSecureStore } from '@/shared/types/auth.interface'
+
+import { errorCatch, getNewTokens } from '@/services/api/helper.auth'
+import { AuthService } from '@/services/auth/auth.service'
+
+import { getAccessToken } from './../../../services/auth/auth.helper'
+
+export const useCheckAuth = (routeName?: string) => {
+	const { user, setUser } = useAuth()
+
+	useEffect(() => {
+		const checkAccessToken = async () => {
+			const accessToken = await getAccessToken()
+			if (accessToken) {
+				try {
+					await getNewTokens()
+				} catch (error) {
+					if (errorCatch(error) === 'jwt expired') {
+						await AuthService.logout()
+						setUser(null)
+					}
+				}
+			}
+		}
+	}, [])
+
+	useEffect(() => {
+		const checkRefreshToken = async () => {
+			const refreshToken = await getItemAsync(EnumSecureStore.REFRESH_TOKEN)
+			if (!refreshToken && user) {
+				await AuthService.logout()
+				setUser(null)
+			}
+		}
+	}, [routeName])
+}
