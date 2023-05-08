48dc2cd378dc36f59f1265568f9bb063b3f6c548
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-08 17:57:45 +0800

    add
    add saveToken

--- cinema-app/app/services/auth/auth.helper.ts
+++ cinema-app/app/services/auth/auth.helper.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { deleteItemAsync, getItemAsync } from 'expo-secure-store'
+import { deleteItemAsync, getItemAsync, setItemAsync } from 'expo-secure-store'
 
 import {
 	EnumAsyncStorage,
@@ -12,6 +12,10 @@ export const getAccessToken = async () => {
 	return accessToken || null
 }
 
+export const saveTokensStorages = async (data: ITokens) => {
+	await setItemAsync(EnumSecureStore.ACCESS_TOKEN, data.accessToken)
+	await setItemAsync(EnumSecureStore.REFRESH_TOKEN, data.refreshToken)
+}
 export const deleteTokensStorages = async (data: ITokens) => {
 	await deleteItemAsync(EnumSecureStore.ACCESS_TOKEN)
 	await deleteItemAsync(EnumSecureStore.REFRESH_TOKEN)
