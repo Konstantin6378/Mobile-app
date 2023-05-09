a9ed5a079ab9f538216fee1878dcc16b7f6b8f78
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-09 20:07:28 +0800

    Update user.service.ts

--- cinema-app/app/services/user.service.ts
+++ cinema-app/app/services/user.service.ts
@@ -1,1 +1,37 @@
-export const UserService = {}
+import { IUser } from '@/shared/types/user.interface'
+
+import { getUsersUrl } from '@/config/api.config'
+
+import { request } from './api/request.api'
+
+export const UserService = {
+	async getAll(searchTerm?: string) {
+		return request<IUser[]>({
+			url: getUsersUrl(''),
+			method: 'GET',
+			params: searchTerm
+				? {
+						searchTerm
+				  }
+				: {}
+		})
+	},
+	async getProfile() {
+		return request<IUser[]>({
+			url: getUsersUrl('/profile'),
+			method: 'GET'
+		})
+	},
+	// async getFavorites() {
+	// 	return request<IMovie[]>({
+	// 		url: getUsersUrl('/profile/favorites'),
+	// 		method: 'GET'
+	// 	})
+	// },
+	async getById(_id: string) {
+		return request<IUser>({
+			url: getUsersUrl(`/${_id}`),
+			method: 'GET'
+		})
+	}
+}
