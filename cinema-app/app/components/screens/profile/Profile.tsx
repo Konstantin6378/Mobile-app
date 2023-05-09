e321ddaee142c74154c9e65235c0143809bbaf16
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-09 20:40:20 +0800

    Update Profile.tsx
    updated

--- cinema-app/app/components/screens/profile/Profile.tsx
+++ cinema-app/app/components/screens/profile/Profile.tsx
@@ -1,18 +1,44 @@
 import { AntDesign } from '@expo/vector-icons'
 import { FC } from 'react'
+import { useForm } from 'react-hook-form'
 import { Pressable, Text, View } from 'react-native'
 
-import { Heading } from '@/components/ui'
+import { Button, Heading, Loader } from '@/components/ui'
 
 import { useAuth } from '@/hooks/useAuth'
 
+import { IAuthFormData } from '@/shared/types/auth.interface'
+
 import { AuthService } from '@/services/auth/auth.service'
+
+import AuthFields from '../auth/AuthFields'
+
+import { useProfile } from './useProfile'
 
 const Profile: FC = () => {
 	const { setUser } = useAuth()
+
+	const { handleSubmit, setValue, control } = useForm<IAuthFormData>({
+		mode: 'onChange'
+	})
+
+	const { isLoading, onSubmit } = useProfile(setValue)
+
 	return (
 		<View className='mt-20 px-3'>
 			<Heading title={'Profile'} />
+			{/* Picture user */}
+
+			{isLoading ? (
+				<Loader />
+			) : (
+				<View className='mt-10'>
+					<AuthFields control={control} />
+					<Button onPress={handleSubmit(onSubmit)} icon={'edit'}>
+						Update profile
+					</Button>
+				</View>
+			)}
 			<Pressable
 				onPress={() => AuthService.logout().then(() => setUser(null))}
 				className='opacity-40 items-center flex-row justify-end'
