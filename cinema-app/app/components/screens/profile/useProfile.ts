f9d5466d1ce7b88608ef898ecb074557799c0d82
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-09 15:43:18 +0800

    add
    add user.service

--- cinema-app/app/components/screens/profile/useProfile.ts
+++ cinema-app/app/components/screens/profile/useProfile.ts
@@ -1,1 +1,8 @@
-export const useProfile = () => {}
+import { useQuery } from '@tanstack/react-query'
+import { UseFormSetValue } from 'react-hook-form'
+
+import { IAuthFormData } from '@/shared/types/auth.interface'
+
+export const useProfile = (setValue: UseFormSetValue<IAuthFormData>) => {
+	// const {} = useQuery()
+}
