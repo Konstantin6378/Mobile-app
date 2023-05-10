15d85b46c1a7a25d3be59144b2ebf352f5c3b5c0
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-10 18:16:50 +0800

    Update index.ts

--- cinema-app/app/components/ui/index.ts
+++ cinema-app/app/components/ui/index.ts
@@ -1,15 +1,16 @@
 export { default as Loader } from './Loader'
 export { default as Button } from './button/Button'
-export { default as BottomMenu } from '../ui/layout/bottom-menu/BottomMenu'
+
 export { default as Toast } from '../ui/Toast'
 export { default as Heading } from '../ui/heading/Heading'
+
 export { default as Layout } from '../ui/layout/Layout'
+export { default as BottomMenu } from '../ui/layout/bottom-menu/BottomMenu'
 
 export { default as DismissKeyboard } from './form-elements/field/DismissKeyboard'
 export { default as Field } from './form-elements/field/Field'
 
 export { default as Auth } from '../screens/auth/Auth'
-
 export { default as Favorites } from '../screens/favorites/Favorites'
 export { default as Home } from '../screens/home/Home'
 export { default as Profile } from '../screens/profile/Profile'
