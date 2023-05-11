11859fc5c0d6c3bff83b489769cda767f5970362
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-11 18:58:40 +0800

    Update index.ts

--- cinema-app/app/components/ui/index.ts
+++ cinema-app/app/components/ui/index.ts
@@ -1,11 +1,11 @@
 export { default as Loader } from './Loader'
 export { default as Button } from './button/Button'
 
-export { default as Toast } from '../ui/Toast'
-export { default as Heading } from '../ui/heading/Heading'
+export { default as Toast } from './Toast'
+export { default as Heading } from './heading/Heading'
 
-export { default as Layout } from '../ui/layout/Layout'
-export { default as BottomMenu } from '../ui/layout/bottom-menu/BottomMenu'
+export { default as Layout } from './layout/Layout'
+export { default as BottomMenu } from './layout/bottom-menu/BottomMenu'
 
 export { default as DismissKeyboard } from './form-elements/field/DismissKeyboard'
 export { default as Field } from './form-elements/field/Field'
@@ -17,4 +17,5 @@ export { default as Profile } from '../screens/profile/Profile'
 export { default as Search } from '../screens/search/Search'
 export { default as Trending } from '../screens/trending/Trending'
 
-export { default as Rating } from '../ui/movie/movie-item/Rating'
+export { default as Rating } from './movie/movie-item/Rating'
+export { default as GenreList } from './movie/movie-item/GenreList'
