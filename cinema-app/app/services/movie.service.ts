e7b49e5a7a6123a8dbfc3c3085e6eed73dd735a2
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-11 17:12:53 +0800

    Update movie.service.ts

--- cinema-app/app/services/movie.service.ts
+++ cinema-app/app/services/movie.service.ts
@@ -1,14 +1,14 @@
 import { IAuthFormData } from '@/shared/types/auth.interface'
 
-import { getMovieIMoviesUrl } from '@/config/api.config'
+import { getMoviesUrl } from '@/config/api.config'
 
 import { IMovie } from './../shared/types/movie.interface'
 import { request } from './api/request.api'
 
-export const MovieIMovieService = {
+export const MovieService = {
 	async getAll(searchTerm?: string) {
 		return request<IMovie[]>({
-			url: getMovieIMoviesUrl(''),
+			url: getMoviesUrl(''),
 			method: 'GET',
 			params: searchTerm
 				? {
@@ -17,40 +17,52 @@ export const MovieIMovieService = {
 				: {}
 		})
 	},
-	async getProfile() {
-		return request<IMovie>({
-			url: getMovieIMoviesUrl('/profile'),
+	async getMostPopularMovies() {
+		return request<IMovie[]>({
+			url: getMoviesUrl('/most-popular'),
 			method: 'GET'
 		})
 	},
 	// async getFavorites() {
 	// 	return request<IMovie[]>({
-	// 		url: getMovieIMoviesUrl('/profile/favorites'),
+	// 		url: getMoviesUrl('/profile/favorites'),
 	// 		method: 'GET'
 	// 	})
 	// },
-	async getById(_id: string) {
+	async getBySlug(slug: string) {
 		return request<IMovie>({
-			url: getMovieIMoviesUrl(`/${_id}`),
+			url: getMoviesUrl(`/by-slug/${slug}`),
+			method: 'GET'
+		})
+	},
+	async getByActor(actorId: string) {
+		return request<IMovie>({
+			url: getMoviesUrl(`/by-actor/${actorId}`),
+			method: 'GET'
+		})
+	},
+	async getByGenres(genresId: string) {
+		return request<IMovie>({
+			url: getMoviesUrl(`/by-genres`),
 			method: 'GET'
 		})
 	},
 	async updateProfile(data: IAuthFormData) {
 		return request<IMovie>({
-			url: getMovieIMoviesUrl(`/profile`),
+			url: getMoviesUrl(`/profile`),
 			method: 'PUT',
 			data
 		})
 	},
-	async deleteMovieIMovie(_id: string) {
+	async deleteMovie(_id: string) {
 		return request<string>({
-			url: getMovieIMoviesUrl(`/${_id}`),
+			url: getMoviesUrl(`/${_id}`),
 			method: 'DELETE'
 		})
 	},
 	async update(_id: string, data: IAuthFormData) {
 		return request<string>({
-			url: getMovieIMoviesUrl(`/${_id}`),
+			url: getMoviesUrl(`/${_id}`),
 
 			method: 'PUT',
 			data
