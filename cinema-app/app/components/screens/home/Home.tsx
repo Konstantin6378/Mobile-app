da9df318f6086df63ea5c872f3eeba5fe0538b40
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-11 17:31:52 +0800

    Update Home.tsx

--- cinema-app/app/components/screens/home/Home.tsx
+++ cinema-app/app/components/screens/home/Home.tsx
@@ -1,23 +1,20 @@
 import { FC } from 'react'
 import { Pressable, Text, View } from 'react-native'
 
-import { Heading, Layout } from '@/components/ui'
+import { Heading, Layout, Loader } from '@/components/ui'
 
 import { useTypedNavigation } from '@/hooks/useTypedNavigation'
 
+import { useGetAllMovies } from './useGetAllMovies'
+
 const Home: FC = () => {
 	const { navigate } = useTypedNavigation()
+
+	const { movies, isLoading } = useGetAllMovies()
+
 	return (
 		<Layout>
-			<Heading title={'Home'} />
-			<Pressable
-				onPress={() => navigate('Auth')}
-				className='opacity-40 items-center flex-row justify-end'
-			>
-				<Text className='text-white text-lg ml-2' style={{ color: 'white' }}>
-					Go to login
-				</Text>
-			</Pressable>
+			{isLoading ? <Loader /> : movies?.length && <Carusel movies={movies} />}
 		</Layout>
 	)
 }
