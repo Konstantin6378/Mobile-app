dd3530a4283f3918680a9ad727f8fcaab1b80d21
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-11 18:57:10 +0800

    Create GenreList.tsx

---
+++ cinema-app/app/components/ui/movie/movie-item/GenreList.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const GenreList: FC = () => {
+	return (
+		<View>
+			<Text>GenreList</Text>
+		</View>
+	)
+}
+export default GenreList
