86c94a092047d4f5970696b46171aeaae617b7c6
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-11 18:53:26 +0800

    Update Rating.tsx

--- cinema-app/app/components/ui/movie/movie-item/Rating.tsx
+++ cinema-app/app/components/ui/movie/movie-item/Rating.tsx
@@ -1,4 +1,5 @@
 import { AntDesign } from '@expo/vector-icons'
+import cn from 'clsx'
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
@@ -13,7 +14,11 @@ const Rating: FC<IRating> = ({ size = 20, rating }) => {
 	return (
 		<View className='flex-row items-center'>
 			<AntDesign name='star' size={size} color={getColor('yellow')} />
-			<Text>Rating</Text>
+			<Text
+				className={cn('text-white ml-2', size === 20 ? 'text-lg' : 'text-base')}
+			>
+				{rating.toFixed(1)}
+			</Text>
 		</View>
 	)
 }
