f51308eaaa0d6e017310f791ef788cd1ab155195
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-13 13:48:11 +0800

    Update carousel.constants.ts

--- cinema-app/app/components/screens/home/carousel/carousel.constants.ts
+++ cinema-app/app/components/screens/home/carousel/carousel.constants.ts
@@ -1,6 +1,6 @@
 import { Dimensions, Platform } from 'react-native'
 
-export const SPACING = 10
+export const SPACING = 20
 const { width } = Dimensions.get('window')
 
 export const ITEM_SIZE = Platform.OS === 'ios' ? width * 0.72 : width * 0.74
