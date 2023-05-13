508d886caada0d6fcad42dd445a149a7a9be0a56
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-13 13:37:01 +0800

    Update Carousel.tsx

--- cinema-app/app/components/screens/home/carousel/Carousel.tsx
+++ cinema-app/app/components/screens/home/carousel/Carousel.tsx
@@ -1,9 +1,10 @@
 import { FC, useRef } from 'react'
-import { Animated, FlatList, View } from 'react-native'
+import { Animated, FlatList, Platform, View } from 'react-native'
 
 import { IMovie } from '@/shared/types/movie.interface'
 
 import CarouselItem from './carousel-item/CarouselItem'
+import { ITEM_SIZE } from './carousel.constants'
 
 const Carousel: FC<{ movies: IMovie[] }> = ({ movies }) => {
 	const scrollX = useRef(new Animated.Value(0)).current
@@ -11,6 +12,17 @@ const Carousel: FC<{ movies: IMovie[] }> = ({ movies }) => {
 		<View>
 			<FlatList
 				data={movies}
+				showsHorizontalScrollIndicator={false}
+				horizontal
+				bounces={false}
+				renderToHardwareTextureAndroid
+				contentContainerStyle={{
+					alignItems: 'center'
+				}}
+				scrollEventThrottle={16}
+				snapToInterval={ITEM_SIZE}
+				snapToAlignment='center'
+				decelerationRate={Platform.OS === 'ios' ? 0 : 0.98}
 				renderItem={({ item: movie, index }) => (
 					<CarouselItem
 						movie={movie}
