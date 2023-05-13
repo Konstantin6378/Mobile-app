5e8c4f69d73210f4cfd6c6d9c9d9a4415e44cb7a
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-13 13:29:08 +0800

    Update Carousel.tsx

--- cinema-app/app/components/screens/home/carousel/Carousel.tsx
+++ cinema-app/app/components/screens/home/carousel/Carousel.tsx
@@ -1,15 +1,21 @@
-import { FC } from 'react'
-import { View } from 'react-native'
+import { FC, useRef } from 'react'
+import { Animated, View } from 'react-native'
 
 import { IMovie } from '@/shared/types/movie.interface'
 
 import CarouselItem from './carousel-item/CarouselItem'
 
 const Carousel: FC<{ movies: IMovie[] }> = ({ movies }) => {
+	const scrollX = useRef(new Animated.Value(0)).current
 	return (
 		<View>
-			{movies.map(movie => (
-				<CarouselItem movie={movie} key={movie._id} />
+			{movies.map((movie, index) => (
+				<CarouselItem
+					movie={movie}
+					key={movie._id}
+					index={index}
+					scrollX={scrollX}
+				/>
 			))}
 		</View>
 	)
