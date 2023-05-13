56b52db55e4743bac2d6687fafb1c760141588a0
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-13 13:11:47 +0800

    Update useItemAnimation.ts

--- cinema-app/app/components/screens/home/carousel/carousel-item/useItemAnimation.ts
+++ cinema-app/app/components/screens/home/carousel/carousel-item/useItemAnimation.ts
@@ -1,1 +1,8 @@
-export const useItemAnimation = () => {}
+import { Animated } from "react-native";
+
+
+export const useItemAnimation = (index: number, scrollX: Animated.Value) => {
+	const inputRange = () => [
+    (index - 2) * 
+  ]
+}
