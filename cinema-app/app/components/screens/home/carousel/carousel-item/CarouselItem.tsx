983712b8de55bb5e868e9f7d873dff115b4e0601
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-13 13:59:02 +0800

    Update CarouselItem.tsx

--- cinema-app/app/components/screens/home/carousel/carousel-item/CarouselItem.tsx
+++ cinema-app/app/components/screens/home/carousel/carousel-item/CarouselItem.tsx
@@ -50,7 +50,7 @@ const CarouselItem: FC<ICarouselItem> = ({ movie, index, scrollX }) => {
 					</View>
 					<Image
 						style={{
-							height: 300,
+							height: ITEM_SIZE * 1.3,
 							resizeMode: 'cover',
 							borderWidth: 1,
 							borderColor: 'white'
