fbc8c3c05f4558750c8181368de6c39e02842765
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-14 18:58:58 +0800

    Update useFavoriteAnimation.ts

--- cinema-app/app/components/ui/movie/movie-item/favorite-button/useFavoriteAnimation.ts
+++ cinema-app/app/components/ui/movie/movie-item/favorite-button/useFavoriteAnimation.ts
@@ -1,1 +1,18 @@
-export const useFavoriteAnimation = () => {}
+import {
+	Extrapolate,
+	interpolate,
+	useAnimatedStyle,
+	useSharedValue
+} from 'react-native-reanimated'
+
+export const useFavoriteAnimation = () => {
+	const liked = useSharedValue(0)
+
+	const outlineStele = useAnimatedStyle(() => ({
+		transform: [
+			{
+				scale: interpolate(liked.value, [0, 1], [1, 0], Extrapolate.CLAMP)
+			}
+		]
+	}))
+}
