2e3f942187effecf717a88adc805b841eae47b05
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-14 18:26:55 +0800

    add
    add file favorite button and useFavorite

---
+++ cinema-app/app/components/ui/movie/movie-item/favorite-button/FavoriteButton.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const FavoriteButton: FC = () => {
+	return (
+		<View>
+			<Text>FavoriteButton</Text>
+		</View>
+	)
+}
+export default FavoriteButton
