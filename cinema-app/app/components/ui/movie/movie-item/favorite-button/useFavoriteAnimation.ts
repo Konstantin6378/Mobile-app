0e4d313b4f74730192c7c87eea3167d506d29811
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-14 19:30:03 +0800

    Update useFavoriteAnimation.ts

--- cinema-app/app/components/ui/movie/movie-item/favorite-button/useFavoriteAnimation.ts
+++ cinema-app/app/components/ui/movie/movie-item/favorite-button/useFavoriteAnimation.ts
@@ -1,12 +1,18 @@
+import { useEffect } from 'react'
 import {
 	Extrapolate,
 	interpolate,
 	useAnimatedStyle,
-	useSharedValue
+	useSharedValue,
+	withSpring
 } from 'react-native-reanimated'
 
-export const useFavoriteAnimation = () => {
+export const useFavoriteAnimation = (isSmashed: boolean) => {
 	const liked = useSharedValue(0)
+
+	useEffect(() => {
+		liked.value = withSpring(isSmashed ? 1 : 0)
+	}, [isSmashed])
 
 	const outlineStyle = useAnimatedStyle(() => ({
 		transform: [
