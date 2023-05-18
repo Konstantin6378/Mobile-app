ad4faa9712cdd33cb0594efc0b51ef91e456e59c
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-18 13:18:00 +0800

    Create MovieItem.tsx

---
+++ cinema-app/app/components/ui/movie/movie-item/MovieItem.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+import {Text, View} from 'react-native'
+
+const MovieItem: FC = () => {
+  return <View>
+          <Text>MovieItem</Text>
+        </View>
+}
+export default MovieItem
