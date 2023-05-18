09ee6d61302248624d453c2015510a182ca79157
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-18 13:29:10 +0800

    Update useMovieItemAnimation.ts

--- cinema-app/app/components/ui/movie/movie-item/useMovieItemAnimation.ts
+++ cinema-app/app/components/ui/movie/movie-item/useMovieItemAnimation.ts
@@ -1,9 +1,15 @@
 import { useEffect } from 'react'
-import { useSharedValue } from 'react-native-reanimated'
+import { useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
 
 export const useMovieItemAnimation = () => {
 	const scale = useSharedValue(0.4)
 	const opacity = useSharedValue(0)
 
 	useEffect(() => {}, [])
+
+	const styleAnimation = useAnimatedStyle(() => ({
+		transform: [{ scale: scale.value }],
+		opacity: opacity.value
+	}))
+	return { styleAnimation }
 }
