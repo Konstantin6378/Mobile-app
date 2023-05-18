d7b5a573c3003f1607e25fb09131de2b50b4d970
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-18 14:02:04 +0800

    Create Description.tsx

---
+++ cinema-app/app/components/ui/heading/Description.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const Description: FC = () => {
+	return (
+		<View>
+			<Text>Description</Text>
+		</View>
+	)
+}
+export default Description
