cc8421b828caf9478ca6726eb29453620d17b230
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-18 13:23:42 +0800

    Update MovieItem.tsx

--- cinema-app/app/components/ui/movie/movie-item/MovieItem.tsx
+++ cinema-app/app/components/ui/movie/movie-item/MovieItem.tsx
@@ -1,9 +1,19 @@
 import { FC } from 'react'
-import {Text, View} from 'react-native'
+import { Text, View } from 'react-native'
+
+import { useTypedNavigation } from '@/hooks/useTypedNavigation'
+import { useTypedRoute } from '@/hooks/useTypedRoute'
 
 const MovieItem: FC = () => {
-  return <View>
-          <Text>MovieItem</Text>
-        </View>
+	const { navigate } = useTypedNavigation()
+	const { name } = useTypedRoute()
+
+	const isFavoritePage = name === 'Favorites'
+
+	return (
+		<View>
+			<Text>MovieItem</Text>
+		</View>
+	)
 }
 export default MovieItem
