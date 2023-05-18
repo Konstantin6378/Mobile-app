9625c140c0d86746a624cb88ac1bbd5e8b98819a
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-18 13:51:59 +0800

    Update Favorites.tsx

--- cinema-app/app/components/screens/favorites/Favorites.tsx
+++ cinema-app/app/components/screens/favorites/Favorites.tsx
@@ -1,14 +1,18 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
-import { Layout } from '@/components/ui'
+import { Layout, Loader } from '@/components/ui'
 import Heading from '@/components/ui/heading/Heading'
 
+import { useFavorites } from './useFavorites'
+
 const Favorites: FC = () => {
+	const { isLoading, favoriteMovies } = useFavorites()
+
+	if (isLoading) return <Loader />
+
 	return (
 		<Layout isHasPadding>
-			<Heading title={'Favorite'} />
-
 			<Text>Favorites</Text>
 		</Layout>
 	)
