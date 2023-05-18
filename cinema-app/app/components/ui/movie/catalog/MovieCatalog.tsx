3f32a58051855c7cf2ac3a3c5b76072f8208a513
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-18 13:52:39 +0800

    Create MovieCatalog.tsx

---
+++ cinema-app/app/components/ui/movie/catalog/MovieCatalog.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const MovieCatalog: FC = () => {
+	return (
+		<View>
+			<Text>MovieCatalog</Text>
+		</View>
+	)
+}
+export default MovieCatalog
