3723b8a952243ca51ad9e28938dc4400d27dc20b
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-18 13:36:08 +0800

    Update MovieItem.tsx

--- cinema-app/app/components/ui/movie/movie-item/MovieItem.tsx
+++ cinema-app/app/components/ui/movie/movie-item/MovieItem.tsx
@@ -5,14 +5,25 @@ import Animated from 'react-native-reanimated'
 import { useTypedNavigation } from '@/hooks/useTypedNavigation'
 import { useTypedRoute } from '@/hooks/useTypedRoute'
 
+import { IMovie } from '@/shared/types/movie.interface'
+
+import { useMovieItemAnimation } from './useMovieItemAnimation'
+
 const ReanimatedPressable = Animated.createAnimatedComponent(Pressable)
 
-const MovieItem: FC = () => {
+interface IMovieItem {
+	index: number
+	className?: string
+	movie: IMovie
+}
+
+const MovieItem: FC<IMovieItem> = ({ index, movie, className }) => {
 	const { navigate } = useTypedNavigation()
 	const { name } = useTypedRoute()
 
 	const isFavoritePage = name === 'Favorites'
 
+	const { styleAnimation } = useMovieItemAnimation(index)
 	// Animations
 
 	return (
