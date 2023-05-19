e444c9813e8226819e70bac8cd46eb5b1ed210f2
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-19 20:30:18 +0800

    Update useGenre.ts

--- cinema-app/app/components/screens/genre/useGenre.ts
+++ cinema-app/app/components/screens/genre/useGenre.ts
@@ -5,8 +5,8 @@ import { useTypedRoute } from '@/hooks/useTypedRoute'
 import { MovieService } from '@/services/movie.service'
 
 export const useGenre = () => {
-	const { params } = useTypedRoute<''>()
-	const { isLoading, data: movies } = useQuery(['get genre movies'], () =>
+	const { params } = useTypedRoute<'Genre'>()
+	const { isLoading, data: movies } = useQuery(['get genre by slug'], () =>
 		MovieService.getMostPopularMovies()
 	)
 	return { movies, isLoading }
