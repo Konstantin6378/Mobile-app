9adf749add82d82961d1097eb080d9af3fc953a5
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-19 20:23:58 +0800

    Update Trending.tsx

--- cinema-app/app/components/screens/trending/Trending.tsx
+++ cinema-app/app/components/screens/trending/Trending.tsx
@@ -1,13 +1,21 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
-import { Heading, Layout, MovieCatalog } from '@/components/ui'
+import { Heading, Layout, Loader, MovieCatalog } from '@/components/ui'
+
+import { useTrending } from './useTrending'
 
 const Trending: FC = () => {
+	const { isLoading, movies } = useTrending()
+	if (isLoading) return <Loader />
 	return (
 		<Layout isHasPadding>
 			<Heading title={'Trending'} />
-			<MovieCatalog title='Trending' />
+			<MovieCatalog
+				title='Trending'
+				movies={movies}
+				description='Trending movies in excellent quality: legal, safe, without ads'
+			/>
 		</Layout>
 	)
 }
