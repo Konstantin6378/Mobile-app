e0034509046f54b9343a2a41cbc5bc93821dad84
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-21 14:27:20 +0800

    Update Movie.tsx

--- cinema-app/app/components/screens/movie/Movie.tsx
+++ cinema-app/app/components/screens/movie/Movie.tsx
@@ -3,6 +3,7 @@ import { Text, View } from 'react-native'
 
 import { Layout, Loader } from '@/components/ui'
 
+import MovieBackground from './MovieBackground'
 import MovieHeader from './MovieHeader'
 import MovieInfo from './MovieInfo'
 import { useMovie } from './useMovie'
@@ -14,6 +15,7 @@ const Movie: FC = () => {
 	return (
 		<Layout style={{ paddingTop: 0 }}>
 			<MovieHeader movie={movie} />
+			<MovieBackground movie={movie} />
 			<MovieInfo movie={movie} />
 		</Layout>
 	)
