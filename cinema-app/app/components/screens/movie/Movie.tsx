ab72c90f6aadcf0789603c34a5fbba8e810564db
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-21 13:55:58 +0800

    Update Movie.tsx

--- cinema-app/app/components/screens/movie/Movie.tsx
+++ cinema-app/app/components/screens/movie/Movie.tsx
@@ -1,11 +1,18 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
+import { Layout, Loader } from '@/components/ui'
+
+import { useMovie } from './useMovie'
+
 const Movie: FC = () => {
+	const { movie, isLoading } = useMovie()
+	if (isLoading) return <Loader />
+	if (!movie) return null
 	return (
-		<View>
+		<Layout style={{ paddingTop: 0 }}>
 			<Text>Movie</Text>
-		</View>
+		</Layout>
 	)
 }
 export default Movie
