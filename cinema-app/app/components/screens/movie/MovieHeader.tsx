141f28c50c006301f09b55012af138fbd022ee64
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-21 14:08:15 +0800

    Update MovieHeader.tsx

--- cinema-app/app/components/screens/movie/MovieHeader.tsx
+++ cinema-app/app/components/screens/movie/MovieHeader.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import { Animated, StyleSheet, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-import { BlurButton, Rating } from '@/components/ui'
+import { BlurButton, FavoriteButton, Rating } from '@/components/ui'
 
 import { useTypedNavigation } from '@/hooks/useTypedNavigation'
 
@@ -24,18 +24,18 @@ const MovieHeader: FC<IMovieHeader> = ({ movie }) => {
 			<Animated.View
 				style={{ ...StyleSheet.absoluteFillObject }}
 				className='bg-[#0D0404]'
-			>
-				<BlurButton icon={'chevron-left'} iconSize={23} onPress={goBack} />
-				<Animated.View className='items-center w-2/3'>
-					<Text
-						className='text-white font-semibold text-2xl mb-0.5 px-2'
-						numberOfLines={1}
-					>
-						{movie.title}
-					</Text>
-					<Rating rating={movie.rating} size={14} />
-				</Animated.View>
+			/>
+			<BlurButton icon={'chevron-left'} iconSize={23} onPress={goBack} />
+			<Animated.View className='items-center w-2/3'>
+				<Text
+					className='text-white font-semibold text-2xl mb-0.5 px-2'
+					numberOfLines={1}
+				>
+					{movie.title}
+				</Text>
+				<Rating rating={movie.rating} size={14} />
 			</Animated.View>
+			<FavoriteButton movieId={movie._id} />
 		</View>
 	)
 }
