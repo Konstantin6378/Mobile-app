794d3e3cd46ac793ae0a727fc4a9d9e61c894cb5
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-21 14:18:16 +0800

    Update MovieInfo.tsx

--- cinema-app/app/components/screens/movie/MovieInfo.tsx
+++ cinema-app/app/components/screens/movie/MovieInfo.tsx
@@ -1,6 +1,6 @@
 import { Entypo } from '@expo/vector-icons'
 import { FC } from 'react'
-import { Animated, Text, View } from 'react-native'
+import { Animated, StyleSheet, Text, View } from 'react-native'
 
 import { Rating } from '@/components/ui'
 
@@ -27,8 +27,24 @@ const MovieInfo: FC<IMovieInfo> = ({ movie }) => {
 					color='rgba(255,255,255,.5)'
 					style={{ marginLeft: 4 }}
 				/>
+				<Text style={styles.text}>{movie.parameters.duration} min.</Text>
+				<Entypo
+					name='dot-single'
+					size={18}
+					color='rgba(255,255,255,.5)'
+					style={{ marginLeft: 4 }}
+				/>
+				<Text style={styles.text}>{movie.parameters.year}</Text>
 			</View>
 		</Animated.View>
 	)
 }
+
+const styles = StyleSheet.create({
+	text: {
+		color: 'white',
+		marginHorizontal: 4,
+		fontSize: 18
+	}
+})
 export default MovieInfo
