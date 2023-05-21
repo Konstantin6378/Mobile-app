1a9e1a997df6080ab5f9c80d21c3d25bf8d9646a
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-21 14:25:56 +0800

    Update MovieBackground.tsx

--- cinema-app/app/components/screens/movie/MovieBackground.tsx
+++ cinema-app/app/components/screens/movie/MovieBackground.tsx
@@ -1,3 +1,4 @@
+import { LinearGradient } from 'expo-linear-gradient'
 import { FC } from 'react'
 import { Animated, Image, StyleSheet, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -18,6 +19,12 @@ const MovieBackground: FC<IMovieBackground> = ({ movie }) => {
 				style={StyleSheet.absoluteFill}
 				resizeMode='cover'
 				source={getMediaSource(movie.poster)}
+			/>
+			<LinearGradient
+				style={StyleSheet.absoluteFill}
+				start={[0, 0.3]}
+				end={[0, 0.8]}
+				colors={['transparent', 'rgba(0,0,0,0.2', '#090909']}
 			/>
 		</Animated.View>
 	)
