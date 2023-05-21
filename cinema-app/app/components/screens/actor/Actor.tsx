782bfde5a81adf605995887efb45c8e3ac1e2e11
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-21 13:48:07 +0800

    Update Actor.tsx

---
+++ cinema-app/app/components/screens/actor/Actor.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const Actor: FC = () => {
+	return (
+		<View>
+			<Text>Actor</Text>
+		</View>
+	)
+}
+export default Actor
