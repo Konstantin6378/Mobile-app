451a09a2d3edf3726c424786ac1e1feed23b5162
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-21 12:59:38 +0800

    Update Search.tsx

--- cinema-app/app/components/screens/search/Search.tsx
+++ cinema-app/app/components/screens/search/Search.tsx
@@ -1,13 +1,27 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
-import { Heading, Layout } from '@/components/ui'
+import { Field, Heading, Layout } from '@/components/ui'
+
+import { ISearchFormData } from './search.interface'
+import { useSearch } from './useSearch'
 
 const Search: FC = () => {
+	const { searchTerm, movies, isLoading, control } = useSearch()
 	return (
 		<Layout isHasPadding>
 			<Heading title={'Search'} />
-			<Text>Search</Text>
+			<View className='mt-3'>
+				<Field<ISearchFormData>
+					placeholder='Type something...'
+					control={control}
+					name='searchTerm'
+					rules={{
+						required: 'Search term is required!'
+					}}
+					keyboardType='web-search'
+				/>
+			</View>
 		</Layout>
 	)
 }
