b5732171cca60bdab1fa073c45dc0829d02824f8
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-21 12:56:06 +0800

    Update useSearch.ts

--- cinema-app/app/components/screens/search/useSearch.ts
+++ cinema-app/app/components/screens/search/useSearch.ts
@@ -1,1 +1,18 @@
-export const useSearch = () => {}
+import { useQuery } from '@tanstack/react-query'
+
+import { MovieService } from '@/services/movie.service'
+
+import { useSearchForm } from './useSearchForm'
+
+export const useSearch = () => {
+	const { searchTerm, debouncedSearch, control } = useSearchForm()
+
+	const { data: movies, isLoading } = useQuery(
+		['search movies', debouncedSearch],
+		() => MovieService.getAll(debouncedSearch),
+		{
+			enabled: !!debouncedSearch
+		}
+	)
+	return { movies, isLoading, searchTerm, control }
+}
