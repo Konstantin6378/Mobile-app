e95f83b44d33225a5e07ddcf61a03849405cff60
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-22 13:24:15 +0800

    Create ActorCarousel.tsx

---
+++ cinema-app/app/components/screens/movie/movie-content/ActorCarousel.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const ActorCarousel: FC = () => {
+	return (
+		<View>
+			<Text>ActorCarousel</Text>
+		</View>
+	)
+}
+export default ActorCarousel
