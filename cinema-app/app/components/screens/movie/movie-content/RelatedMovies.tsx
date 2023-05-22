f188defd4d4cb4c4ae7a4b90fcc3a8190cb1e300
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-22 14:05:34 +0800

    Update RelatedMovies.tsx

--- cinema-app/app/components/screens/movie/movie-content/RelatedMovies.tsx
+++ cinema-app/app/components/screens/movie/movie-content/RelatedMovies.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
+import { Loader } from '@/components/ui'
+
+import { useGetRelatedMovies } from './useRelatedMovies'
+
 interface IRelatedMovies {
 	genreIds: string[]
 	currentMovieId: string
 }
 
 const RelatedMovies: FC<IRelatedMovies> = ({ currentMovieId, genreIds }) => {
+	const { data, isLoading } = useGetRelatedMovies(genreIds, currentMovieId)
+
+	if (isLoading) return <Loader />
+	if (!data?.length) return null
 	return (
 		<View>
 			<Text>RelatedMovies</Text>
