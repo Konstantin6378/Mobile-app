c7c5182eae2fb29adf1e7358483efeb1d27720ef
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-22 13:34:05 +0800

    Update MovieContent.tsx

--- cinema-app/app/components/screens/movie/movie-content/MovieContent.tsx
+++ cinema-app/app/components/screens/movie/movie-content/MovieContent.tsx
@@ -2,8 +2,6 @@ import { useScrollToTop } from '@react-navigation/native'
 import { FC, useRef } from 'react'
 import { Animated, ScrollView, Text } from 'react-native'
 
-import { IMovie } from '@/shared/types/movie.interface'
-
 import { IMovieComponent } from '../movie-page.interface'
 
 import MovieInfo from './MovieInfo'
