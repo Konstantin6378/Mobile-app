8c4e11fc9add57adadb5dab198e288e287cf829d
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-22 13:21:41 +0800

    Create MovieContent.tsx

---
+++ cinema-app/app/components/screens/movie/MovieContent.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const MovieContent: FC = () => {
+	return (
+		<View>
+			<Text>MovieContent</Text>
+		</View>
+	)
+}
+export default MovieContent
