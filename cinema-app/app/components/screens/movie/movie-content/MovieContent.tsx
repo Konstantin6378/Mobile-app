b9aea66cc2f130c51ba138632cc2a7deab061baa
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-22 13:29:38 +0800

    Update MovieContent.tsx

--- cinema-app/app/components/screens/movie/movie-content/MovieContent.tsx
+++ cinema-app/app/components/screens/movie/movie-content/MovieContent.tsx
@@ -1,11 +1,27 @@
-import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { useScrollToTop } from '@react-navigation/native'
+import { FC, useRef } from 'react'
+import { Animated, ScrollView, Text } from 'react-native'
+
+import { IMovie } from '@/shared/types/movie.interface'
+
+import MovieInfo from './MovieInfo'
+
+interface IMovieContent {
+	movie: IMovie
+}
 
 const MovieContent: FC = () => {
+	const ref = useRef<ScrollView>(null)
+	useScrollToTop(ref)
+
 	return (
-		<View>
-			<Text>MovieContent</Text>
-		</View>
+		<Animated.ScrollView
+			ref={ref}
+			showsVerticalScrollIndicator={false}
+			scrollEventThrottle={16}
+		>
+			<MovieInfo movie={movie} />
+		</Animated.ScrollView>
 	)
 }
 export default MovieContent
