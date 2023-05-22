6532aacdda207e5f1e0d47790636caf9de2df012
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-22 13:51:17 +0800

    Update ActorCarousel.tsx

--- cinema-app/app/components/screens/movie/movie-content/ActorCarousel.tsx
+++ cinema-app/app/components/screens/movie/movie-content/ActorCarousel.tsx
@@ -1,16 +1,31 @@
-import { useNavigation } from '@react-navigation/native'
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { ListRenderItemInfo, Pressable } from 'react-native'
+
+import HorizontalList from '@/components/ui/HorizontalList'
+
+import { useTypedNavigation } from '@/hooks/useTypedNavigation'
 
 import { IActor } from '@/shared/types/actor.interface'
 
 const ActorCarousel: FC<{ actors: IActor[] }> = ({ actors }) => {
-	const { navigate } = useNavigation()
+	const { navigate } = useTypedNavigation()
 
 	return (
-		<View>
-			<Text>ActorCarousel</Text>
-		</View>
+		<HorizontalList
+			data={actors}
+			// @ts-ignore
+			renderItem={({ item: actor }: ListRenderItemInfo<IActor>) => (
+				<Pressable
+					onPress={() =>
+						navigate('Actor', {
+							slug: actor.slug
+						})
+					}
+					className='flex-row items-center rounded-xl overflow-hidden w-48 mr-4'
+					style={{ height: 72, backgroundColor: 'rgba(255, 255, 255, 0.07' }}
+				></Pressable>
+			)}
+		/>
 	)
 }
 export default ActorCarousel
