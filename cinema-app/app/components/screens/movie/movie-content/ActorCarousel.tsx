90a4c11c4ff31bfc2eda522054832dbd610abda3
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-22 13:55:19 +0800

    Update ActorCarousel.tsx

--- cinema-app/app/components/screens/movie/movie-content/ActorCarousel.tsx
+++ cinema-app/app/components/screens/movie/movie-content/ActorCarousel.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
-import { ListRenderItemInfo, Pressable } from 'react-native'
+import { Image, ListRenderItemInfo, Pressable, Text, View } from 'react-native'
 
 import HorizontalList from '@/components/ui/HorizontalList'
 
 import { useTypedNavigation } from '@/hooks/useTypedNavigation'
 
 import { IActor } from '@/shared/types/actor.interface'
+
+import { getMediaSource } from '@/utils/getMediaSource'
 
 const ActorCarousel: FC<{ actors: IActor[] }> = ({ actors }) => {
 	const { navigate } = useTypedNavigation()
@@ -23,7 +25,22 @@ const ActorCarousel: FC<{ actors: IActor[] }> = ({ actors }) => {
 					}
 					className='flex-row items-center rounded-xl overflow-hidden w-48 mr-4'
 					style={{ height: 72, backgroundColor: 'rgba(255, 255, 255, 0.07' }}
-				></Pressable>
+				>
+					<Image
+						className='h-full'
+						style={{ width: 50, resizeMode: 'cover' }}
+						source={getMediaSource(actor.photo)}
+					/>
+					<View className='p-3 w-11/12'>
+						<Text
+							className='text-white text-base font-medium pr-7
+            '
+							numberOfLines={1}
+						>
+							{actor.name}
+						</Text>
+					</View>
+				</Pressable>
 			)}
 		/>
 	)
