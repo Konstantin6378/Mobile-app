d274f36ea17353ac08b3fb32837cf5e59fdc033f
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 18:29:50 +0700

    Update Actor.tsx

--- cinema-app/app/components/screens/actor/Actor.tsx
+++ cinema-app/app/components/screens/actor/Actor.tsx
@@ -2,10 +2,22 @@ import { FC } from 'react'
 import { Text, View } from 'react-native'
 
 const Actor: FC = () => {
+	const { isLoading, movies, genre } = useGenre()
+	if (isLoading) return <Loader />
 	return (
-		<View>
-			<Text>Actor</Text>
-		</View>
+		<Layout isHasPadding>
+			<Heading title={'Genre'} />
+			{genre ? (
+				<MovieCatalog
+					title={genre.name}
+					movies={movies}
+					description={genre.description}
+					isBackButton
+				/>
+			) : (
+				<NotFound />
+			)}
+		</Layout>
 	)
 }
 export default Actor
