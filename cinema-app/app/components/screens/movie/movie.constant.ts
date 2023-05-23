32adac931af1a41e5ca5d94134bea8a8a53b069c
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 13:11:57 +0800

    Update movie.constant.ts

--- cinema-app/app/components/screens/movie/movie.constant.ts
+++ cinema-app/app/components/screens/movie/movie.constant.ts
@@ -1,7 +1,3 @@
-import { Dimensions } from 'react-native'
-
-const { height } = Dimensions.get('window')
-
 export const HEADER_HEIGHT = 350
 
 export const inputRange = [-HEADER_HEIGHT, 0, HEADER_HEIGHT]
