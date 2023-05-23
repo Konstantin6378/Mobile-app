1833738cc08cea3e447f783e1b1de691a051cbe0
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 18:51:00 +0700

    Create UsersList.tsx

---
+++ cinema-app/app/components/screens/admin/users/UsersList.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const UsersList: FC = () => {
+	return (
+		<View>
+			<Text>UsersList</Text>
+		</View>
+	)
+}
+export default UsersList
