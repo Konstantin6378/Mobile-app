532614573a0a183fb767beb4215cab4a403ad618
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 18:51:43 +0700

    Create MoviesList.tsx

---
+++ cinema-app/app/components/screens/admin/movies/MoviesList.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const MoviesList: FC = () => {
+	return (
+		<View>
+			<Text>MoviesList</Text>
+		</View>
+	)
+}
+export default MoviesList
