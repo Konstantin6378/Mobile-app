a2a79b4024f02dd0eff481150a5e2d6fedbde21c
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 18:47:57 +0700

    Create MovieEdit.tsx

---
+++ cinema-app/app/components/screens/admin/movie/MovieEdit.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const MovieEdit: FC = () => {
+	return (
+		<View>
+			<Text>MovieEdit</Text>
+		</View>
+	)
+}
+export default MovieEdit
