67c870e816cdca75b0dfb33350a2e77b2c94afc1
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 18:48:31 +0700

    Create GenreEdit.tsx

---
+++ cinema-app/app/components/screens/admin/genre/GenreEdit.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const GenreEdit: FC = () => {
+	return (
+		<View>
+			<Text>GenreEdit</Text>
+		</View>
+	)
+}
+export default GenreEdit
