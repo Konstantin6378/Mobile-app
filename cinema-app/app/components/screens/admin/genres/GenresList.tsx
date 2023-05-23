f87d0d16c0d39b012ffa69d0fd1248a9757b7fa1
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 18:52:38 +0700

    Create GenresList.tsx

---
+++ cinema-app/app/components/screens/admin/genres/GenresList.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const GenresList: FC = () => {
+	return (
+		<View>
+			<Text>GenresList</Text>
+		</View>
+	)
+}
+export default GenresList
