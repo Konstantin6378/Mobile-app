8ed11547b9e4262b9a2c3ef1ad26cd361c5438cb
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 18:52:24 +0700

    Create ActorsList.tsx

---
+++ cinema-app/app/components/screens/admin/actors/ActorsList.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const ActorsList: FC = () => {
+	return (
+		<View>
+			<Text>ActorsList</Text>
+		</View>
+	)
+}
+export default ActorsList
