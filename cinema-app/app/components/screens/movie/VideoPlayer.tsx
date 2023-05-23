ef3e82c2065b5161f40e3bbfa8e9f48d649e4d44
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 14:31:05 +0700

    Update VideoPlayer.tsx

--- cinema-app/app/components/screens/movie/VideoPlayer.tsx
+++ cinema-app/app/components/screens/movie/VideoPlayer.tsx
@@ -1,14 +1,22 @@
-import { FC } from 'react'
+import { Video } from 'expo-av'
+import { FC, useRef } from 'react'
 import { Text, View } from 'react-native'
 
 import { Button } from '@/components/ui'
 
+import { getMediaSource } from '@/utils/getMediaSource'
+
 const VideoPlayer: FC<{ video: string }> = ({ video }) => {
+	const videoRef = useRef<Video>(null)
+
 	return (
 		<>
 			<Button icon='play' className='mb-6'>
 				Watch movie
 			</Button>
+			<View>
+				<Video ref={videoRef} source={getMediaSource(video)} />
+			</View>
 		</>
 	)
 }
