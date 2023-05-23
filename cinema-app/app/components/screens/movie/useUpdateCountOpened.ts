453351d32b0b532fb10d60730be732c6c4eb1718
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-23 15:00:09 +0700

    Update useUpdateCountOpened.ts

--- cinema-app/app/components/screens/movie/useUpdateCountOpened.ts
+++ cinema-app/app/components/screens/movie/useUpdateCountOpened.ts
@@ -1,1 +1,17 @@
-export const useUpdateCountOpened = () => {}
+import { useMutation } from '@tanstack/react-query'
+import { useEffect } from 'react'
+
+import { useTypedRoute } from '@/hooks/useTypedRoute'
+
+import { MovieService } from '@/services/movie.service'
+
+export const useUpdateCountOpened = () => {
+	const { params } = useTypedRoute<'Movie'>()
+	const { mutate } = useMutation(['update count opened'], () =>
+		MovieService.updateCountOpened(params.slug)
+	)
+
+	useEffect(() => {
+		mutate()
+	}, [])
+}
