ee4b979130336a685b3ad62b54c0b4dee7b51285
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-26 11:59:02 +0700

    Create AdminNavigation.tsx

---
+++ cinema-app/app/components/ui/admin-navigation/AdminNavigation.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const AdminNavigation: FC = () => {
+	return (
+		<View>
+			<Text>AdminNavigation</Text>
+		</View>
+	)
+}
+export default AdminNavigation
