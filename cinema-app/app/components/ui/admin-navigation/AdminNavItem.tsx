aff73c139a14d782b68e1c1979304dbf594a9cd5
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-26 12:28:06 +0700

    Update AdminNavItem.tsx

--- cinema-app/app/components/ui/admin-navigation/AdminNavItem.tsx
+++ cinema-app/app/components/ui/admin-navigation/AdminNavItem.tsx
@@ -1,5 +1,7 @@
+import { MaterialIcons } from '@expo/vector-icons'
+import cn from 'clsx'
 import { FC } from 'react'
-import { Pressable, Text, View } from 'react-native'
+import { Pressable, Text } from 'react-native'
 
 import { useTypedNavigation } from '@/hooks/useTypedNavigation'
 import { useTypedRoute } from '@/hooks/useTypedRoute'
@@ -17,7 +19,18 @@ const AdminNavItem: FC<{ item: INavItem }> = ({
 			onPress={() => navigate(routeName)}
 			className='flex-row items-center mb-1'
 		>
-			<Text>AdminNavItem</Text>
+			<MaterialIcons
+				name={icon}
+				size={18}
+				color={name === routeName ? '#d73033' : '#666'}
+			/>
+			<Text
+				className={cn('text-xl text-[#d1d1d1] ml-2', {
+					'text-[#d73033 font-medium ': name === routeName
+				})}
+			>
+				{title}
+			</Text>
 		</Pressable>
 	)
 }
