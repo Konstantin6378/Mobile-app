7f945427d591db0406727a34274f12c5a0a82d20
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-26 12:15:10 +0700

    Update admin-navigation.data.ts

---
+++ cinema-app/app/components/ui/admin-navigation/admin-navigation.data.ts
@@ -0,0 +1,9 @@
+import { INavItem } from './admin-navigation.interface'
+
+export const navItems: INavItem[] = [
+	{
+		icon: 'insert-chart-outlined',
+		title: 'Statistics',
+		routeName: 'Admin'
+	}
+]
