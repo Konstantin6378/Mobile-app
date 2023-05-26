5bab39b0e41f5ad29b1231ecf8bf7afdf3968143
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-26 12:17:49 +0700

    Update admin-navigation.data.ts

--- cinema-app/app/components/ui/admin-navigation/admin-navigation.data.ts
+++ cinema-app/app/components/ui/admin-navigation/admin-navigation.data.ts
@@ -5,5 +5,25 @@ export const navItems: INavItem[] = [
 		icon: 'insert-chart-outlined',
 		title: 'Statistics',
 		routeName: 'Admin'
+	},
+	{
+		icon: 'group',
+		title: 'Users',
+		routeName: 'UsersList'
+	},
+	{
+		icon: 'movie-filter',
+		title: 'Movies',
+		routeName: 'MoviesList'
+	},
+	{
+		icon: 'recent-actors',
+		title: 'Actors',
+		routeName: 'ActorsList'
+	},
+	{
+		icon: 'category',
+		title: 'Genres',
+		routeName: 'GenresList'
 	}
 ]
