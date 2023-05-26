a1947b5df7910cd6e34943c0d0771850179a558c
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-26 12:19:36 +0700

    Create AdminNavItem.tsx

---
+++ cinema-app/app/components/ui/admin-navigation/AdminNavItem.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const AdminNavItem: FC = () => {
+	return (
+		<View>
+			<Text>AdminNavItem</Text>
+		</View>
+	)
+}
+export default AdminNavItem
