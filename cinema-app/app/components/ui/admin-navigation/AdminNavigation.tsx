111f30fc2987381338427310e8fd9fb7b071b881
Konstantin6378 <sisyphus7863@yandex.ru>
2023-05-26 12:05:53 +0700

    Update AdminNavigation.tsx

--- cinema-app/app/components/ui/admin-navigation/AdminNavigation.tsx
+++ cinema-app/app/components/ui/admin-navigation/AdminNavigation.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
+import BlurButton from '../blur-button/BlurButton'
 import Heading from '../heading/Heading'
 
 interface IAdminNavigation {
@@ -9,8 +10,18 @@ interface IAdminNavigation {
 }
 const AdminNavigation: FC<IAdminNavigation> = ({ title, isBackButton }) => {
 	return (
-		<View>
+		<View className='flex-row justify-between items-center z-10 mb-5'>
 			<Heading title={title} />
+			<View className='relative flex-row'>
+				{isBackButton && (
+					<BlurButton
+						icon={'chevron-left'}
+						iconSize={24}
+						className='w-12 h-12 mr-3'
+						onPress={goBack}
+					/>
+				)}
+			</View>
 		</View>
 	)
 }
