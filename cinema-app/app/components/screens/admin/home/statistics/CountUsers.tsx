fdba3ba12e168b7abddc144ee340d740edab0d25
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-31 16:47:06 +0700

    Update CountUsers.tsx

--- cinema-app/app/components/screens/admin/home/statistics/CountUsers.tsx
+++ cinema-app/app/components/screens/admin/home/statistics/CountUsers.tsx
@@ -2,10 +2,10 @@ import {FC} from 'react'
 import {View, Text} from 'react-native'
 
 const CountUsers: FC = () => {
-return (
-<View>
-<Text>CountUsers</Text>
-</View>
-)
+  return (
+      <View>
+         <Text>CountUsers</Text>
+      </View>
+  )
 }
 export default CountUsers
