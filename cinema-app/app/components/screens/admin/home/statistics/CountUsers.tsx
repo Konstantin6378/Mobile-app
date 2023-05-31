d0a34e841f5be6a2765a7904e85da02a34014a78
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-31 16:45:14 +0700

    add

---
+++ cinema-app/app/components/screens/admin/home/statistics/CountUsers.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const CountUsers: FC = () => {
+return (
+<View>
+<Text>CountUsers</Text>
+</View>
+)
+}
+export default CountUsers
