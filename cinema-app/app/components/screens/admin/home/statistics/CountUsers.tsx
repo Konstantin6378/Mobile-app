307a1a8f0e79b8462024e40c474b3f1acb1365d0
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-31 17:49:31 +0700

    Update CountUsers.tsx

--- cinema-app/app/components/screens/admin/home/statistics/CountUsers.tsx
+++ cinema-app/app/components/screens/admin/home/statistics/CountUsers.tsx
@@ -1,7 +1,10 @@
+import { UserService } from '@/services/user.service'
+import { useQuery } from '@tanstack/react-query'
 import {FC} from 'react'
 import {View, Text} from 'react-native'
 
 const CountUsers: FC = () => {
+    const {isLoading, data} = useQuery(['get count users'], () => UserService.get)
   return (
       <View>
          <Text>CountUsers</Text>
