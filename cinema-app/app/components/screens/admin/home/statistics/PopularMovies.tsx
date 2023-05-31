5309fa8219f6bf13db2d5b2f461cc5d86d450e69
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-31 16:45:29 +0700

    Update PopularMovies.tsx

---
+++ cinema-app/app/components/screens/admin/home/statistics/PopularMovies.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const PopularMovies: FC = () => {
+return (
+<View>
+<Text>PopularMovies</Text>
+</View>
+)
+}
+export default PopularMovies
