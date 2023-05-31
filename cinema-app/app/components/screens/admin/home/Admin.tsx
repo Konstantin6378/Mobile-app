e00d405b930b5bafe443a4c136f3278b90f5fc7e
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-31 16:49:15 +0700

    Update Admin.tsx

--- cinema-app/app/components/screens/admin/home/Admin.tsx
+++ cinema-app/app/components/screens/admin/home/Admin.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react'
 
 import { AdminNavigation, Layout } from '@/components/ui'
+import { ScrollView } from 'react-native';
+import Statistics from './statistics/Statistics';
 
 const Admin: FC = () => {
 	return (
 		<Layout isHasPadding>
 			<AdminNavigation title='Statistics' />
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<Statistics/>
+			</ScrollView>
 		</Layout>
 	)
 }
