412d5faa83fdace88151ffab1a3efa4f4e4b20da
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-31 16:44:33 +0700

    Create Statistics.tsx

---
+++ cinema-app/app/components/screens/admin/home/statistics/Statistics.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+const Statistics: FC = () => {
+	return (
+		<View>
+			<Text>Statistics</Text>
+		</View>
+	)
+}
+export default Statistics
