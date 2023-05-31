1884b696ba9d42e91ae25392a3c570dfcd9c6019
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-05-31 21:15:44 +0700

    Update PopularMovies.tsx

--- cinema-app/app/components/screens/admin/home/statistics/PopularMovies.tsx
+++ cinema-app/app/components/screens/admin/home/statistics/PopularMovies.tsx
@@ -1,13 +1,30 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
 import { useTrending } from '@/components/screens/trending/useTrending'
-import {FC} from 'react'
-import {View, Text} from 'react-native'
+import { Loader } from '@/components/ui'
+
+import { STYLE_BLOCK } from './statistics.styles'
 
 const PopularMovies: FC = () => {
-  const {movies, isLoading} = useTrending(2)
-  return (
-      <View>
-         <Text>PopularMovies</Text>
-      </View>
-  )
+	const { movies, isLoading } = useTrending(2)
+	return (
+		<View className={`${STYLE_BLOCK} mt-6`}>
+			<Text className='text-2xl font-bold text-white text-center mb-2'>
+				The most popular movies
+			</Text>
+			<View className='flex-row justify-between mt-2'>
+				{isLoading ? (
+					<Loader />
+				) : movies?.length ? (
+					movies.map(movie => (
+						<View key={movie._id} style={{ width: '47%' }}></View>
+					))
+				) : (
+					<Text className='text-white text-lg'>Elements not found</Text>
+				)}
+			</View>
+		</View>
+	)
 }
 export default PopularMovies
