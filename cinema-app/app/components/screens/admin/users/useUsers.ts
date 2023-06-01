296f93e33d306e888ffdb9dba5b35c9e2cfcb7fc
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-01 15:19:45 +0700

    Update useUsers.ts

--- cinema-app/app/components/screens/admin/users/useUsers.ts
+++ cinema-app/app/components/screens/admin/users/useUsers.ts
@@ -1,8 +1,19 @@
 import { useTypedNavigation } from "@/hooks/useTypedNavigation"
 import { useSearchForm } from "../../search/useSearchForm"
+import { useQuery } from "@tanstack/react-query"
+import { UserService } from "@/services/user.service"
 
 export const useUsers = () => {
     const {control, debouncedSearch} = useSearchForm()
 
     const {navigate} = useTypedNavigation()
+
+    const queryData = useQuery(
+        ['search users', debouncedSearch],
+        () => UserService.getAll(debouncedSearch),
+        {
+            enabled: !!debouncedSearch,
+            select: data => 
+        }
+    )
  }
