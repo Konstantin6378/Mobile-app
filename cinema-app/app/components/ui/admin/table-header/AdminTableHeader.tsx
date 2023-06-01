efc48671c13ed367448cc4c0b5774f6efb61a2ef
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-01 15:00:04 +0700

    Create AdminTableHeader.tsx

---
+++ cinema-app/app/components/ui/admin/table-header/AdminTableHeader.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const AdminTableHeader: FC = () => {
+  return (
+      <View>
+         <Text>AdminTableHeader</Text>
+      </View>
+  )
+}
+export default AdminTableHeader
