e79f750a8d400a4b89eb2855ea5169d97278c431
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-01 15:24:55 +0700

    Update useUsers.ts

--- cinema-app/app/components/screens/admin/users/useUsers.ts
+++ cinema-app/app/components/screens/admin/users/useUsers.ts
@@ -1,19 +1,37 @@
-import { useTypedNavigation } from "@/hooks/useTypedNavigation"
-import { useSearchForm } from "../../search/useSearchForm"
-import { useQuery } from "@tanstack/react-query"
-import { UserService } from "@/services/user.service"
+import { useQuery } from '@tanstack/react-query'
+
+import { ITableItem } from '@/components/ui/admin/table/admin-table.interface'
+
+import { useTypedNavigation } from '@/hooks/useTypedNavigation'
+
+import { UserService } from '@/services/user.service'
+
+import { useSearchForm } from '../../search/useSearchForm'
 
 export const useUsers = () => {
-    const {control, debouncedSearch} = useSearchForm()
+	const { control, debouncedSearch } = useSearchForm()
 
-    const {navigate} = useTypedNavigation()
+	const { navigate } = useTypedNavigation()
 
-    const queryData = useQuery(
-        ['search users', debouncedSearch],
-        () => UserService.getAll(debouncedSearch),
-        {
-            enabled: !!debouncedSearch,
-            select: data => 
-        }
-    )
- }
+	const queryData = useQuery(
+		['search users', debouncedSearch],
+		() => UserService.getAll(debouncedSearch),
+		{
+			enabled: !!debouncedSearch,
+			select: data =>
+				data.map(
+					(user): ITableItem => ({
+						_id: user._id,
+						editNavigate: () =>
+							navigate('UserEdit', {
+								id: user._id
+							}),
+						items: [
+							user.email,
+							new Date(user.createdAt).toLocaleDateString('ru')
+						]
+					})
+				)
+		}
+	)
+}
