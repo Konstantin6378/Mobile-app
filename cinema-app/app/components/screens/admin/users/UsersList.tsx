d7e57932fd6c88cfc117d4d9ac8bd0b9c959bbe9
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-01 15:14:50 +0700

    Update UsersList.tsx

--- cinema-app/app/components/screens/admin/users/UsersList.tsx
+++ cinema-app/app/components/screens/admin/users/UsersList.tsx
@@ -1,11 +1,14 @@
 import { AdminNavigation, Layout } from '@/components/ui'
+import AdminTableHeader from '@/components/ui/admin/table-header/AdminTableHeader'
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
 const UsersList: FC = () => {
+	const {} = useUsers()
 	return (
 		<Layout isHasPadding>
 			<AdminNavigation title='UserList'/>
+			<AdminTableHeader control={contol}/>
 		</Layout>
 	)
 }
