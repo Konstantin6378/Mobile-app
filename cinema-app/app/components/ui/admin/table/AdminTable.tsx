212e78ffa54a214e71b846bdbeac0a28f7abdb0a
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-02 13:04:40 +0700

    Update AdminTable.tsx

--- cinema-app/app/components/ui/admin/table/AdminTable.tsx
+++ cinema-app/app/components/ui/admin/table/AdminTable.tsx
@@ -1,17 +1,21 @@
-import {FC} from 'react'
-import {View, Text, ScrollView} from 'react-native'
-import { IAdminTable } from './admin-table.interface'
+import { FC } from 'react'
+import { ScrollView, Text, View } from 'react-native'
+
 import AdminTableHeader from './AdminTableHeader'
+import { IAdminTable } from './admin-table.interface'
 
-const AdminTable: FC<IAdminTable> = ({isLoading, headerItems, removeHandler, tableItems}) => {
-  return (
-     <ScrollView showsHorizontalScrollIndicator={false} horizontal>
-      <View className='pb-6'>
-        <AdminTableHeader headerItems={headerItems}/>
-
-      </View>
-
-     </ScrollView>
-  )
+const AdminTable: FC<IAdminTable> = ({
+	isLoading,
+	headerItems = [],
+	removeHandler,
+	tableItems
+}) => {
+	return (
+		<ScrollView showsHorizontalScrollIndicator={false} horizontal>
+			<View className='pb-6'>
+				<AdminTableHeader headerItems={headerItems} />
+			</View>
+		</ScrollView>
+	)
 }
 export default AdminTable
