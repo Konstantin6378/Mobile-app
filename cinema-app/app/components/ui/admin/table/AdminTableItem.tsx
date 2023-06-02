6cb3d96d1cab721bbd31467d9cf7f1fe36be8c95
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-02 13:10:34 +0700

    Create AdminTableItem.tsx

---
+++ cinema-app/app/components/ui/admin/table/AdminTableItem.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const AdminTableItem: FC = () => {
+  return (
+      <View>
+         <Text>AdminTableItem</Text>
+      </View>
+  )
+}
+export default AdminTableItem
