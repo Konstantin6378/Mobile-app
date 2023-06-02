c5ef9af3d4dae98ee259cc38278921ccaf90fd2d
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-02 13:34:23 +0700

    Update AdminTable.tsx

--- cinema-app/app/components/ui/admin/table/AdminTable.tsx
+++ cinema-app/app/components/ui/admin/table/AdminTable.tsx
@@ -1,14 +1,15 @@
 import { FC } from 'react'
 import { ScrollView, Text, View } from 'react-native'
 
-import AdminTableHeader from './AdminTableHeader'
-import { IAdminTable } from './admin-table.interface'
 import Loader from '../../Loader'
+
+import AdminTableHeader from './AdminTableHeader'
 import AdminTableItem from './AdminTableItem'
+import { IAdminTable } from './admin-table.interface'
 
 const AdminTable: FC<IAdminTable> = ({
 	isLoading,
-	headerItems ,
+	headerItems,
 	removeHandler,
 	tableItems = []
 }) => {
@@ -16,13 +17,21 @@ const AdminTable: FC<IAdminTable> = ({
 		<ScrollView showsHorizontalScrollIndicator={false} horizontal>
 			<View className='pb-6'>
 				<AdminTableHeader headerItems={headerItems} />
-        {isLoading ? <Loader /> : tableItems?.length ? tableItems.map(tableItem => (
-          <AdminTableItem 
-            key={tableItem._id}
-            removeHandler={() => removeHandler(tableItem._id)}
-            tableItem={tableItem}
-          />
-        )): <Text className='text-white text-lg'>Elements not found</Text>}
+				{isLoading ? (
+					<Loader />
+				) : tableItems?.length ? (
+					<ScrollView showsVerticalScrollIndicator={false}>
+						{tableItems.map(tableItem => (
+							<AdminTableItem
+								key={tableItem._id}
+								removeHandler={() => removeHandler(tableItem._id)}
+								tableItem={tableItem}
+							/>
+						))}
+					</ScrollView>
+				) : (
+					<Text className='text-white text-lg'>Elements not found</Text>
+				)}
 			</View>
 		</ScrollView>
 	)
