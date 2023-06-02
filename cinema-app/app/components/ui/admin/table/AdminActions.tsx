cb2bff05ba0b0e4f9e1ad5792b73706e06576525
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-02 13:17:01 +0700

    Create AdminActions.tsx

---
+++ cinema-app/app/components/ui/admin/table/AdminActions.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const AdminActions: FC = () => {
+  return (
+      <View>
+         <Text>AdminActions</Text>
+      </View>
+  )
+}
+export default AdminActions
