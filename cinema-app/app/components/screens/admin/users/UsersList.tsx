0e4c02b9d7e10737fd13f5bd1a61bf9f2ede11f1
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-02 13:02:38 +0700

    Update UsersList.tsx

--- cinema-app/app/components/screens/admin/users/UsersList.tsx
+++ cinema-app/app/components/screens/admin/users/UsersList.tsx
@@ -3,13 +3,15 @@ import AdminTableHeader from '@/components/ui/admin/table-header/AdminHeader'
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 import { useUsers } from './useUsers'
+import AdminTable from '@/components/ui/admin/table/AdminTable'
 
 const UsersList: FC = () => {
-	const {control, deleteItemAsync, isLoading, data} = useUsers()
+	const {control, deleteAsync, isLoading, data} = useUsers()
 	return (
 		<Layout isHasPadding>
 			<AdminNavigation title='UserList'/>
 			<AdminTableHeader control={control}/>
+			<AdminTable tableItems={} isLoading={isLoading} headerItems={['Email', 'Date register']} removeHandler={deleteAS} />
 		</Layout>
 	)
 }
