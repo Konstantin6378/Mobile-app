c769e03e28e702410e2f68b2fd190f55befde4c7
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-02 13:04:49 +0700

    Update admin-table.interface.ts

--- cinema-app/app/components/ui/admin/table/admin-table.interface.ts
+++ cinema-app/app/components/ui/admin/table/admin-table.interface.ts
@@ -1,16 +1,16 @@
-export interface ITableItem  {
-    _id: string
-    editNavigate: () => void
-    items: string[]
+export interface ITableItem {
+	_id: string
+	editNavigate: () => void
+	items: string[]
 }
-export interface IAdminTableItem  {
-    tableItem: ITableItem
-    removeHandler: () => void
+export interface IAdminTableItem {
+	tableItem: ITableItem
+	removeHandler: () => void
 }
 
-export interface IAdminTable  {
-    tableItems?: ITableItem[]
-    isLoading: boolean
-    headerItems: string[]
-    removeHandler: (id: string) => void
+export interface IAdminTable {
+	tableItems?: ITableItem[]
+	isLoading: boolean
+	headerItems: string[]
+	removeHandler: (id: string) => void
 }
