c455d2997caa8dc795ebebae073de2e2cc5f44fd
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-02 13:05:39 +0700

    Update UsersList.tsx

--- cinema-app/app/components/screens/admin/users/UsersList.tsx
+++ cinema-app/app/components/screens/admin/users/UsersList.tsx
@@ -1,17 +1,23 @@
-import { AdminNavigation, Layout } from '@/components/ui'
-import AdminTableHeader from '@/components/ui/admin/table-header/AdminHeader'
 import { FC } from 'react'
 
-import { useUsers } from './useUsers'
+import { AdminNavigation, Layout } from '@/components/ui'
+import AdminTableHeader from '@/components/ui/admin/table-header/AdminHeader'
 import AdminTable from '@/components/ui/admin/table/AdminTable'
 
+import { useUsers } from './useUsers'
+
 const UsersList: FC = () => {
-	const {control, deleteAsync, isLoading, data} = useUsers()
+	const { control, deleteAsync, isLoading, data } = useUsers()
 	return (
 		<Layout isHasPadding>
-			<AdminNavigation title='UserList'/>
-			<AdminTableHeader control={control}/>
-			<AdminTable tableItems={data} isLoading={isLoading} headerItems={['Email', 'Date register']} removeHandler={deleteAsync} />
+			<AdminNavigation title='UserList' />
+			<AdminTableHeader control={control} />
+			<AdminTable
+				tableItems={data}
+				isLoading={isLoading}
+				headerItems={['Email', 'Date register']}
+				removeHandler={deleteAsync}
+			/>
 		</Layout>
 	)
 }
