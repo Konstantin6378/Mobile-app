f060c3003be41e16f04a0b59620a2131cab837ac
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 14:21:38 +0700

    Update useActors.tsx

--- cinema-app/app/components/screens/admin/actors/useActors.tsx
+++ cinema-app/app/components/screens/admin/actors/useActors.tsx
@@ -1,11 +1,74 @@
-import {FC} from 'react'
-import {View, Text} from 'react-native'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
+import { Toast } from 'react-native-toast-message/lib/src/Toast'
 
-const useActors: FC = () => {
-  return (
-      <View>
-         <Text>useActors</Text>
-      </View>
-  )
+import { ITableItem } from '@/components/ui/admin/table/admin-table.interface'
+
+import { useTypedNavigation } from '@/hooks/useTypedNavigation'
+
+import {MovieService } from '@/services/movie.service'
+
+import { useSearchForm } from '../../search/useSearchForm'
+
+export const useUsers = () => {
+	const { control, debouncedSearch } = useSearchForm()
+
+	const { navigate } = useTypedNavigation()
+
+	const queryData = useQuery(
+		['search movies', debouncedSearch],
+		() =>MovieService.getAll(debouncedSearch),
+		{
+			select: data =>
+				data.map(
+					(movie): ITableItem => ({
+						_id: movie._id,
+						editNavigate: () =>
+							navigate('UserEdit', {
+								id: movie._id
+							}),
+						items: [
+							movie.title,
+                            `${movie.genres[0].name} ${movie.genres.length > 1 ? '...' : ''}`,
+                            String(movie.rating)
+                        ]
+					})
+				)
+		}
+	)
+	const { mutateAsync: createAsync } = useMutation(
+		['create movie'],
+		() =>MovieService.create(),
+		{
+			onSuccess: async (_id) => {
+				Toast.show({
+					type: 'success',
+					text1: 'Delete movie',
+					text2: 'delete was successful'
+				})
+				navigate('MovieEdit', {
+                    id: _id
+                })
+			}
+		}
+	)
+	const { mutateAsync: deleteAsync } = useMutation(
+		['delete movie'],
+		(movieId: string) =>MovieService.delete(movieId),
+		{
+			onSuccess: async () => {
+				Toast.show({
+					type: 'success',
+					text1: 'Delete movie',
+					text2: 'delete was successful'
+				})
+				await queryData.refetch()
+			}
+		}
+	)
+
+	return useMemo(
+		() => ({ ...queryData, control, deleteAsync, createAsync }),
+		[queryData, deleteAsync, createAsync]
+	)
 }
-export default useActors
