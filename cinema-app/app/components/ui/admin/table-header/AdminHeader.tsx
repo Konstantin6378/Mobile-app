a017a84a4f23bffa0b88d660911099e47a4339df
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 14:55:43 +0700

    Update AdminHeader.tsx

--- cinema-app/app/components/ui/admin/table-header/AdminHeader.tsx
+++ cinema-app/app/components/ui/admin/table-header/AdminHeader.tsx
@@ -1,30 +1,35 @@
-import { ISearchFormData } from '@/components/screens/search/search.interface'
-import {FC} from 'react'
+import { FC } from 'react'
 import { Control } from 'react-hook-form'
-import {View} from 'react-native'
+import { View } from 'react-native'
+
+import { ISearchFormData } from '@/components/screens/search/search.interface'
+
 import Field from '../../form-elements/field/Field'
+
 import AdminCreateButton from './AdminCreateButton'
 
-interface IAdminTableHeader  {
-    onPress?: () => void
-    control: Control<ISearchFormData>
+interface IAdminTableHeader {
+	onPress?: () => void
+	control: Control<ISearchFormData>
 }
 
-const AdminTableHeader: FC<IAdminTableHeader> = ({control,onPress}) => {
-  return (
-      <View className='flex-row items-center justify-between mb-3'>
-        <View style={{
-            width: onPress ? '82%' : '100%'
-        }}>
-            <Field<ISearchFormData>
-                placeholder='Type something...'
-                control={control}
-                name='searchTerm'
-                keyboardType='web-search'
-            />
-            {onPress && <AdminCreateButton onPress={onPress}/>}
-        </View>
-      </View>
-  )
+const AdminTableHeader: FC<IAdminTableHeader> = ({ control, onPress }) => {
+	return (
+		<View className='flex-row items-center justify-between mb-3'>
+			<View
+				style={{
+					width: onPress ? '82%' : '100%'
+				}}
+			>
+				<Field<ISearchFormData>
+					placeholder='Type something...'
+					control={control}
+					name='searchTerm'
+					keyboardType='web-search'
+				/>
+				{onPress && <AdminCreateButton onPress={onPress} />}
+			</View>
+		</View>
+	)
 }
 export default AdminTableHeader
