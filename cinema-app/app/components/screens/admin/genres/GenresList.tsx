63479c91a299d52ce48c227cf4b977bda57c5bc3
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 14:43:34 +0700

    Update GenresList.tsx

--- cinema-app/app/components/screens/admin/genres/GenresList.tsx
+++ cinema-app/app/components/screens/admin/genres/GenresList.tsx
@@ -1,11 +1,24 @@
 import { FC } from 'react'
-import { Text, View } from 'react-native'
 
-const GenresList: FC = () => {
+import { AdminNavigation, Layout } from '@/components/ui'
+import AdminTableHeader from '@/components/ui/admin/table-header/AdminHeader'
+import AdminTable from '@/components/ui/admin/table/AdminTable'
+
+import { useMovies } from './useMovies'
+
+const MoviesList: FC = () => {
+	const { control, deleteAsync, isLoading, data } = useMovies()
 	return (
-		<View>
-			<Text>GenresList</Text>
-		</View>
+		<Layout isHasPadding>
+			<AdminNavigation title='Movies' />
+			<AdminTableHeader control={control} />
+			<AdminTable
+				tableItems={data}
+				isLoading={isLoading}
+				headerItems={['Title', 'Main genre', 'Rating']}
+				removeHandler={deleteAsync}
+			/>
+		</Layout>
 	)
 }
-export default GenresList
+export default MoviesList
