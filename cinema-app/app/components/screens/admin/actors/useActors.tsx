f4b8eed971842b5c89d795b70fa3193ec0c47ac6
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 14:14:12 +0700

    Create useActors.tsx

---
+++ cinema-app/app/components/screens/admin/actors/useActors.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const useActors: FC = () => {
+  return (
+      <View>
+         <Text>useActors</Text>
+      </View>
+  )
+}
+export default useActors
