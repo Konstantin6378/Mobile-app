02a8e40d8263c1685aa241cf86ca91ea6803d0da
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 14:40:56 +0700

    Update MoviesList.tsx

--- cinema-app/app/components/screens/admin/movies/MoviesList.tsx
+++ cinema-app/app/components/screens/admin/movies/MoviesList.tsx
@@ -1,11 +1,24 @@
 import { FC } from 'react'
-import { Text, View } from 'react-native'
 
-const MoviesList: FC = () => {
+import { AdminNavigation, Layout } from '@/components/ui'
+import AdminTableHeader from '@/components/ui/admin/table-header/AdminHeader'
+import AdminTable from '@/components/ui/admin/table/AdminTable'
+
+import { useUsers } from './useUsers'
+
+const UsersList: FC = () => {
+	const { control, deleteAsync, isLoading, data } = useUsers()
 	return (
-		<View>
-			<Text>MoviesList</Text>
-		</View>
+		<Layout isHasPadding>
+			<AdminNavigation title='Users' />
+			<AdminTableHeader control={control} />
+			<AdminTable
+				tableItems={data}
+				isLoading={isLoading}
+				headerItems={['Email', 'Date register']}
+				removeHandler={deleteAsync}
+			/>
+		</Layout>
 	)
 }
-export default MoviesList
+export default UsersList
