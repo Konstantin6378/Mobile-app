66a8b04f99785f96f84e15605440e70f864a43d2
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 18:46:59 +0700

    Update useUserEdit.ts

--- cinema-app/app/hooks/useUserEdit.ts
+++ cinema-app/app/hooks/useUserEdit.ts
@@ -1,6 +1,17 @@
 import { IUserEditInput } from "@/shared/types/user.interface";
 import { UseFormSetValue } from "react-hook-form";
+import { useTypedRoute } from "./useTypedRoute";
+import { useQuery } from "@tanstack/react-query";
+import { UserService } from "@/services/user.service";
 
 export const useUserEdit = (setValue: UseFormSetValue<IUserEditInput>) => {
- 
- }
+    const {params} = useTypedRoute<'UserEdit'>()
+    const userId= params.id
+
+    const {isLoading} = useQuery(['get user', userId], () => UserService.getById(userId), {
+        onSuccess(data){
+            setValue('email', data.email)
+            setValue('isAdmin', data.isAdmin)
+        }
+    })
+}
