996f0f0c3b6b00f6e03067c5b84f3f7e87bdd9c2
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 20:28:35 +0700

    Update UserEdit.tsx

--- cinema-app/app/components/screens/admin/user/UserEdit.tsx
+++ cinema-app/app/components/screens/admin/user/UserEdit.tsx
@@ -1,36 +1,53 @@
-import { AdminNavigation, Button, Layout, Loader } from '@/components/ui'
-import { useUserEdit } from '@/components/screens/admin/user/useUserEdit'
-import { IUserEditInput } from '@/shared/types/user.interface'
 import { FC } from 'react'
-import { useForm, Control, Controller } from 'react-hook-form'
-import {  Pressable, Text, View } from 'react-native'
-import AuthFields from '../../auth/AuthFields'
+import { Control, Controller, useForm } from 'react-hook-form'
+import { Pressable, Text, View } from 'react-native'
+
+import { useUserEdit } from '@/components/screens/admin/user/useUserEdit'
+import { AdminNavigation, Button, Layout, Loader } from '@/components/ui'
+
 import { IAuthFormData } from '@/shared/types/auth.interface'
+import { IUserEditInput } from '@/shared/types/user.interface'
+
+import AuthFields from '../../auth/AuthFields'
 
 const UserEdit: FC = () => {
-	const {control, setValue, handleSubmit} = useForm<IUserEditInput>({
+	const { control, setValue, handleSubmit } = useForm<IUserEditInput>({
 		mode: 'onChange'
 	})
 
-	const {isLoading, onSubmit} = useUserEdit(setValue)
+	const { isLoading, onSubmit } = useUserEdit(setValue)
 
 	return (
 		<Layout isHasPadding>
 			<AdminNavigation title='Edit user' isBackButton />
 			<View>
-				{isLoading ? <Loader/> : <>
-				<AuthFields control={control as unknown as Control<IAuthFormData>} />
-				<Controller control={control} name='isAdmin' render={({field: {onChange, value}}) => (
-					<Pressable onPress={() => onChange(!value)} className='my-5 w-40'>
-						<Text className='underline text-white text-base'>
-							{value ? 'Make it regular user' : 'Make it admin'}
-						</Text>
-					</Pressable>
-				)} />
-				<Button onPress={handleSubmit(onSubmit)} icon='pen-tool'>
-					Update
-				</Button>
-				</>}
+				{isLoading ? (
+					<Loader />
+				) : (
+					<>
+						<AuthFields
+							control={control as unknown as Control<IAuthFormData>}
+						/>
+						<Controller
+							control={control}
+							name='isAdmin'
+							render={({ field: { onChange, value } }) => (
+								<Pressable
+									onPress={() => onChange(!value)}
+									className='my-5 w-40'
+								>
+									<CheckBox />
+									<Text className='underline text-white text-base'>
+										{value ? 'Make it regular user' : 'Make it admin'}
+									</Text>
+								</Pressable>
+							)}
+						/>
+						<Button onPress={handleSubmit(onSubmit)} icon='pen-tool'>
+							Update
+						</Button>
+					</>
+				)}
 			</View>
 		</Layout>
 	)
