0f9af54419fa4d97b398f5aaac5d5f3582063439
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 19:10:00 +0700

    add

--- cinema-app/app/components/screens/admin/actor/ActorEdit.tsx
+++ cinema-app/app/components/screens/admin/actor/ActorEdit.tsx
@@ -1,24 +1,24 @@
 import { AdminNavigation, Layout, Loader } from '@/components/ui'
-import { useUserEdit } from '@/components/screens/admin/user/useUserEdit'
-import { IUserEditInput } from '@/shared/types/user.interface'
+import { useACtorEdit } from '@/components/screens/admin/actor/useACtorEdit'
+import { IACtorEditInput } from '@/shared/types/actor.interface'
 import { FC } from 'react'
 import { useForm } from 'react-hook-form'
 import {  View } from 'react-native'
 
-const UserEdit: FC = () => {
-	const {control, setValue, handleSubmit} = useForm<IUserEditInput>({
+const ACtorEdit: FC = () => {
+	const {control, setValue, handleSubmit} = useForm<IACtorEditInput>({
 		mode: 'onChange'
 	})
 
-	const {isLoading, onSubmit} = useUserEdit(setValue)
+	const {isLoading, onSubmit} = useACtorEdit(setValue)
 
 	return (
 		<Layout isHasPadding>
-			<AdminNavigation title='Edit user' isBackButton />
+			<AdminNavigation title='Edit actor' isBackButton />
 			<View>
 				{isLoading ? <Loader/> : <></>}
 			</View>
 		</Layout>
 	)
 }
-export default UserEdit
+export default ACtorEdit
