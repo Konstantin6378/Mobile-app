b426cb08aeff8faa4f0be5f34f4042a2dc78a49f
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 19:08:59 +0700

    Update ActorEdit.tsx

--- cinema-app/app/components/screens/admin/actor/ActorEdit.tsx
+++ cinema-app/app/components/screens/admin/actor/ActorEdit.tsx
@@ -1,11 +1,24 @@
+import { AdminNavigation, Layout, Loader } from '@/components/ui'
+import { useUserEdit } from '@/components/screens/admin/user/useUserEdit'
+import { IUserEditInput } from '@/shared/types/user.interface'
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { useForm } from 'react-hook-form'
+import {  View } from 'react-native'
 
-const ActorEdit: FC = () => {
+const UserEdit: FC = () => {
+	const {control, setValue, handleSubmit} = useForm<IUserEditInput>({
+		mode: 'onChange'
+	})
+
+	const {isLoading, onSubmit} = useUserEdit(setValue)
+
 	return (
-		<View>
-			<Text>ActorEdit</Text>
-		</View>
+		<Layout isHasPadding>
+			<AdminNavigation title='Edit user' isBackButton />
+			<View>
+				{isLoading ? <Loader/> : <></>}
+			</View>
+		</Layout>
 	)
 }
-export default ActorEdit
+export default UserEdit
