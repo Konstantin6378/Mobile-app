37fa3f0fc3b9e257b32ee0c842695efc78b91009
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 18:57:44 +0700

    Update UserEdit.tsx

--- cinema-app/app/components/screens/admin/user/UserEdit.tsx
+++ cinema-app/app/components/screens/admin/user/UserEdit.tsx
@@ -1,7 +1,16 @@
+import { useUserEdit } from '@/hooks/useUserEdit'
+import { IUserEditInput } from '@/shared/types/user.interface'
 import { FC } from 'react'
+import { useForm } from 'react-hook-form'
 import { Text, View } from 'react-native'
 
 const UserEdit: FC = () => {
+	const {control, setValue, handleSubmit} = useForm<IUserEditInput>({
+		mode: 'onChange'
+	})
+
+	const {isLoading, onSubmit} = useUserEdit(setValue)
+
 	return (
 		<View>
 			<Text>UserEdit</Text>
