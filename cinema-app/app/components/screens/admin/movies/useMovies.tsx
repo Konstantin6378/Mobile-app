3570b0b70e9877e8d4a1896297fbe0f393c606c1
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 14:13:45 +0700

    Create useMovies.tsx

---
+++ cinema-app/app/components/screens/admin/movies/useMovies.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const useMovies: FC = () => {
+  return (
+      <View>
+         <Text>useMovies</Text>
+      </View>
+  )
+}
+export default useMovies
