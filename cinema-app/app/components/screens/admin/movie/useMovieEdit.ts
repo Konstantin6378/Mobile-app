a363f66d01bd5083a4a9ae1463b92dbb6da662c5
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 19:59:27 +0700

    Create useMovieEdit.ts

---
+++ cinema-app/app/components/screens/admin/movie/useMovieEdit.ts
@@ -0,0 +1,38 @@
+import { IActorEditInput } from "@/shared/types/actor.interface";
+import { SubmitHandler, UseFormSetValue } from "react-hook-form";
+import { useTypedRoute } from "@/hooks/useTypedRoute";
+import { useMutation, useQuery, QueryClient, useQueryClient } from '@tanstack/react-query';
+import { ActorService } from "@/services/actor.service";
+import { Toast } from "react-native-toast-message/lib/src/Toast";
+
+export const useActorEdit = (setValue: UseFormSetValue<IActorEditInput>) => {
+    const {params} = useTypedRoute<'ActorEdit'>()
+    const actorId= params.id
+
+    const {isLoading} = useQuery(['get actor', actorId], () => ActorService.getById(actorId), {
+        onSuccess(data){
+            setValue('email', data.email)
+            setValue('isAdmin', data.isAdmin)
+        },
+        enabled: !!actorId
+    })
+
+    const {invalidateQueries} = useQueryClient()
+
+    const {mutateAsync} = useMutation(['update actor'], (data:IActorEditInput) => ActorService.update(actorId, data), 
+    {
+        async onSuccess() {
+            Toast.show({
+                type: 'success',
+                text1: 'Updated actor',
+                text2: 'update was successful'
+            })
+
+            await invalidateQueries(['search actors'])
+        }
+    })
+    const onSubmit: SubmitHandler<IActorEditInput> = async data => {
+        await mutateAsync(data)
+    }
+    return{onSubmit, isLoading}
+}
