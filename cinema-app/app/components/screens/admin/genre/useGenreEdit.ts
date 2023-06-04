c7d4d24cce7846b7f3a48f19e211aef36f5964f3
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 20:13:14 +0700

    Update useGenreEdit.ts

--- cinema-app/app/components/screens/admin/genre/useGenreEdit.ts
+++ cinema-app/app/components/screens/admin/genre/useGenreEdit.ts
@@ -1,52 +1,52 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { SubmitHandler, UseFormSetValue } from "react-hook-form";
-import { Toast } from "react-native-toast-message/lib/src/Toast";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { SubmitHandler, UseFormSetValue } from 'react-hook-form'
+import { Toast } from 'react-native-toast-message/lib/src/Toast'
 
+import { useTypedRoute } from '@/hooks/useTypedRoute'
 
+import { IGenreEditInput } from '@/shared/types/genre.interface'
 
-import { useTypedRoute } from "@/hooks/useTypedRoute";
+import { GenreService } from '@/services/genre.service'
 
-
-
-import { IGenreEditInput } from "@/shared/types/genre.interface";
-
-
-
-import { GenreService } from "@/services/genre.service";
-
+import { getKeys } from '@/utils/getKeys'
 
 export const useGenreEdit = (setValue: UseFormSetValue<IGenreEditInput>) => {
-    const {params} = useTypedRoute<'GenreEdit'>()
-    const genreId= params.id
+	const { params } = useTypedRoute<'GenreEdit'>()
+	const genreId = params.id
 
-    const { isLoading } = useQuery(
-			['get genre', genreId],
-			() => GenreService.getById(genreId),
-			{
-				onSuccess(data) {
-					setValue('email', data.email)
-					setValue('isAdmin', data.isAdmin)
-				},
-				enabled: !!genreId
+	const { isLoading } = useQuery(
+		['get genre', genreId],
+		() => GenreService.getById(genreId),
+		{
+			onSuccess(data) {
+				// Object.entries<string>(data).find(([key, value]) => {})
+				getKeys(data).forEach(key => {
+					setValue(key, data[key])
+				})
+			},
+			enabled: !!genreId
+		}
+	)
+
+	const { invalidateQueries } = useQueryClient()
+
+	const { mutateAsync } = useMutation(
+		['update genre'],
+		(data: IGenreEditInput) => GenreService.update(genreId, data),
+		{
+			async onSuccess() {
+				Toast.show({
+					type: 'success',
+					text1: 'Updated genre',
+					text2: 'update was successful'
+				})
+
+				await invalidateQueries(['search genres'])
 			}
-		)
-
-    const {invalidateQueries} = useQueryClient()
-
-    const {mutateAsync} = useMutation(['update genre'], (data:IGenreEditInput) => GenreService.update(genreId, data), 
-    {
-        async onSuccess() {
-            Toast.show({
-                type: 'success',
-                text1: 'Updated genre',
-                text2: 'update was successful'
-            })
-
-            await invalidateQueries(['search genres'])
-        }
-    })
-    const onSubmit: SubmitHandler<IGenreEditInput> = async data => {
-        await mutateAsync(data)
-    }
-    return{onSubmit, isLoading}
+		}
+	)
+	const onSubmit: SubmitHandler<IGenreEditInput> = async data => {
+		await mutateAsync(data)
+	}
+	return { onSubmit, isLoading }
 }
