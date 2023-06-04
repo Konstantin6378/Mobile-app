26af168907a089e85edd638f444219d60d6e86f3
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 14:15:06 +0700

    Create useGenres.tsx

---
+++ cinema-app/app/components/screens/admin/genres/useGenres.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const useGenres: FC = () => {
+  return (
+      <View>
+         <Text>useGenres</Text>
+      </View>
+  )
+}
+export default useGenres
