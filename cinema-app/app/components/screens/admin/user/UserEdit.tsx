11079ea31ac44640018b1b4905a3ef98877e47d5
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 20:18:06 +0700

    Update UserEdit.tsx

--- cinema-app/app/components/screens/admin/user/UserEdit.tsx
+++ cinema-app/app/components/screens/admin/user/UserEdit.tsx
@@ -2,8 +2,10 @@ import { AdminNavigation, Layout, Loader } from '@/components/ui'
 import { useUserEdit } from '@/components/screens/admin/user/useUserEdit'
 import { IUserEditInput } from '@/shared/types/user.interface'
 import { FC } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, Control } from 'react-hook-form'
 import {  View } from 'react-native'
+import AuthFields from '../../auth/AuthFields'
+import { IAuthFormData } from '@/shared/types/auth.interface'
 
 const UserEdit: FC = () => {
 	const {control, setValue, handleSubmit} = useForm<IUserEditInput>({
@@ -16,7 +18,9 @@ const UserEdit: FC = () => {
 		<Layout isHasPadding>
 			<AdminNavigation title='Edit user' isBackButton />
 			<View>
-				{isLoading ? <Loader/> : <></>}
+				{isLoading ? <Loader/> : <>
+				<AuthFields control={control as unknown as Control<IAuthFormData>} />
+				</>}
 			</View>
 		</Layout>
 	)
