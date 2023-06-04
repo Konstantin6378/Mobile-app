ed2e32d7e639eef9a1071d95bb9bcb6541bb484d
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-04 20:26:10 +0700

    Update UserEdit.tsx

--- cinema-app/app/components/screens/admin/user/UserEdit.tsx
+++ cinema-app/app/components/screens/admin/user/UserEdit.tsx
@@ -2,8 +2,8 @@ import { AdminNavigation, Button, Layout, Loader } from '@/components/ui'
 import { useUserEdit } from '@/components/screens/admin/user/useUserEdit'
 import { IUserEditInput } from '@/shared/types/user.interface'
 import { FC } from 'react'
-import { useForm, Control } from 'react-hook-form'
-import {  View } from 'react-native'
+import { useForm, Control, Controller } from 'react-hook-form'
+import {  Pressable, Text, View } from 'react-native'
 import AuthFields from '../../auth/AuthFields'
 import { IAuthFormData } from '@/shared/types/auth.interface'
 
@@ -20,7 +20,13 @@ const UserEdit: FC = () => {
 			<View>
 				{isLoading ? <Loader/> : <>
 				<AuthFields control={control as unknown as Control<IAuthFormData>} />
-
+				<Controller control={control} name='isAdmin' render={({field: {onChange, value}}) => (
+					<Pressable onPress={() => onChange(!value)} className='my-5 w-40'>
+						<Text className='underline text-white text-base'>
+							{value ? 'Make it regular user' : 'Make it admin'}
+						</Text>
+					</Pressable>
+				)} />
 				<Button onPress={handleSubmit(onSubmit)} icon='pen-tool'>
 					Update
 				</Button>
