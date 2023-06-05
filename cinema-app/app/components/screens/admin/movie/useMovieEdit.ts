7dc6e84a4c449804bb48765a15c13e78f034a065
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-05 13:08:38 +0700

    Update useMovieEdit.ts

--- cinema-app/app/components/screens/admin/movie/useMovieEdit.ts
+++ cinema-app/app/components/screens/admin/movie/useMovieEdit.ts
@@ -1,4 +1,4 @@
-import { QueryClient, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { SubmitHandler, UseFormSetValue } from "react-hook-form";
 import { Toast } from "react-native-toast-message/lib/src/Toast";
 
