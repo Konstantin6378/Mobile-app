4406075a6280aeda5574494f554e466781315d68
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-05 14:10:16 +0700

    Update useUploadMedia.ts

--- cinema-app/app/components/ui/form-elements/upload-field/useUploadMedia.ts
+++ cinema-app/app/components/ui/form-elements/upload-field/useUploadMedia.ts
@@ -1,5 +1,10 @@
+import { useMutation } from "@tanstack/react-query";
+import { TypeUploadMedia } from "./upload-field.interface";
+import { useState } from 'react';
 
 
 export const useUploadMedia:TypeUploadMedia = (onChange, folder)  => {
+    const [isLoading, setIsLoading] = useState(false)
 
+    const {mutateAsync} = useMutation(['upload file'], () => )
 }
