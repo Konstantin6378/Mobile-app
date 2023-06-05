66b0919c9d6efe7765d632a14cbd1643c3c0f6ed
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-05 15:38:09 +0700

    Update Genre.tsx

--- cinema-app/app/components/screens/genre/Genre.tsx
+++ cinema-app/app/components/screens/genre/Genre.tsx
@@ -1,29 +1,34 @@
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 
 import { Heading, Layout, Loader, MovieCatalog } from '@/components/ui'
 import NotFound from '@/components/ui/NotFound'
 
 import { useGenre } from './useGenre'
 import { ScrollView, Animated } from 'react-native';
+import { useScrollToTop } from '@react-navigation/native'
 
 const Genre: FC = () => {
 	const { isLoading, movies, genre } = useGenre()
+	const ref = useRef<ScrollView>(null)
+	useScrollToTop(ref)
 	if (isLoading) return <Loader />
 	return (
 		<Layout isHasPadding>
 			<Heading title={'Genre'} />
-			{genre ? (
-				<Animated.ScrollView showsVerticalScrollIndicator={false}>
-				<MovieCatalog
-					title={genre.name}
-					movies={movies}
-					description={genre.description}
-					isBackButton
+			<Animated.ScrollView ref={ref}
+			showsVerticalScrollIndicator={false}
+			scrollEventThrottle={16}>
+				{genre ? (
+					<MovieCatalog
+						title={genre.name}
+						movies={movies}
+						description={genre.description}
+						isBackButton
 					/>
-					</Animated.ScrollView>
-			) : (
-				<NotFound />
-			)}
+				) : (
+					<NotFound />
+				)}
+			</Animated.ScrollView>
 		</Layout>
 	)
 }
