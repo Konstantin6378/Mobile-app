9d95e60333640ac88013515c339f568c599a6ba4
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-05 14:48:33 +0700

    Update UploadField.tsx

--- cinema-app/app/components/ui/form-elements/upload-field/UploadField.tsx
+++ cinema-app/app/components/ui/form-elements/upload-field/UploadField.tsx
@@ -1,9 +1,13 @@
+import cn from 'clsx'
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+
+import { getMediaSource } from '@/utils/getMediaSource'
+
+import Loader from '../../Loader'
 
 import { IUploadField } from './upload-field.interface'
 import { useUploadMedia } from './useUploadMedia'
-import cn from 'clsx';
 
 const UploadField: FC<IUploadField> = ({
 	onChange,
@@ -15,16 +19,33 @@ const UploadField: FC<IUploadField> = ({
 	value,
 	gradient = ['#1db052', '#178d42']
 }) => {
-    const {isLoading, uploadFile} = useUploadMedia(onChange, folder)
+	const { isLoading, uploadFile } = useUploadMedia(onChange, folder)
 
 	return (
 		<View
-            className={cn(
-                'bg-[#232323] w-full border rounded-lg py-2.5 px-4 my-1.5',
-                error ? 'border-red' : 'border-transparent'
-            )}
-        >
-			<Text>UploadField</Text>
+			className={cn(
+				'bg-[#232323] w-full border rounded-lg py-2.5 px-4 my-1.5',
+				error ? 'border-red' : 'border-transparent'
+			)}
+			style={style}
+		>
+			<View className='flex-row justify-between items-center'>
+				<View></View>
+				{!isNoImage && (
+					<View className='w-24 h-24 rounded-2xl overflow-hidden ml-5'>
+						{isLoading ? (
+							<Loader />
+						) : (
+							!!value && (
+								<Image
+									source={getMediaSource(value)}
+									className='w-full h-full'
+								/>
+							)
+						)}
+					</View>
+				)}
+			</View>
 		</View>
 	)
 }
