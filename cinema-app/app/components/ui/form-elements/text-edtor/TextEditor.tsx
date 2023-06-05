011285bc2979f6c55e883ba87b2ace923a691a30
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-05 12:11:38 +0700

    Update TextEditor.tsx

--- cinema-app/app/components/ui/form-elements/text-edtor/TextEditor.tsx
+++ cinema-app/app/components/ui/form-elements/text-edtor/TextEditor.tsx
@@ -1,18 +1,29 @@
-import { FC, useRef } from 'react';
-import {View, Text} from 'react-native'
+import cn from 'clsx'
+import { FC, useRef } from 'react'
+import { Text, View } from 'react-native'
+import { RichEditor } from 'react-native-pell-rich-editor'
+
 import { ITextEditor } from './text-editor.interface'
 
 const TextEditor: FC<ITextEditor> = ({
-    value,
-    onChange,
-    placeholder = 'Write your cool content here 😊',
-    error
+	value,
+	onChange,
+	placeholder = 'Write your cool content here 😊',
+	error
 }) => {
-    const richEditor = useRef(null)
-  return (
-      <View>
-         <Text>TextEditor</Text>
-      </View>
-  )
+	const richEditor = useRef<RichEditor>(null)
+	return (
+		<View>
+			<View
+				className={cn(
+					'my-4 border flex-col-reverse w-full border-solid border-transparent rounded-xl overflow-hidden',
+					{
+						'border-red': !!error
+					}
+				)}
+			></View>
+            {error && <Text className='text-red'>{error.message}</Text>}
+		</View>
+	)
 }
 export default TextEditor
