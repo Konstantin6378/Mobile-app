4f205482b74dcfc621e0fa2254e0adb12c7f76cb
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-05 14:03:54 +0700

    Update upload-field.interface.ts

--- cinema-app/app/components/ui/form-elements/upload-field/upload-field.interface.ts
+++ cinema-app/app/components/ui/form-elements/upload-field/upload-field.interface.ts
@@ -1,2 +1,13 @@
+import { FieldError } from "react-hook-form";
+import { ViewStyle } from "react-native";
+
 export interface IUploadField  {
+    folder?: string;
+    value?: string;
+    onChange: (... events: any[]) => void;
+    placeholder: string;
+    error?: FieldError
+    style?: ViewStyle
+    isNoImage?: boolean;
+    gradient?: [string, string];
 }
