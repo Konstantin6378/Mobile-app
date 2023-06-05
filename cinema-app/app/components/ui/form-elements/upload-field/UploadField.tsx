14ddb64a929033e659d7dabff50d88ed6fbd1df3
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-05 14:40:40 +0700

    Create UploadField.tsx

---
+++ cinema-app/app/components/ui/form-elements/upload-field/UploadField.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const UploadField: FC = () => {
+  return (
+      <View>
+         <Text>UploadField</Text>
+      </View>
+  )
+}
+export default UploadField
