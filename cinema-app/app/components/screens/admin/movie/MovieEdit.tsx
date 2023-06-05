fd973eff0838ab3ad82d834726cdfd316e6218f4
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-05 21:55:31 +0700

    Update MovieEdit.tsx

--- cinema-app/app/components/screens/admin/movie/MovieEdit.tsx
+++ cinema-app/app/components/screens/admin/movie/MovieEdit.tsx
@@ -1,12 +1,13 @@
-import { AdminNavigation, Button, Layout, Loader } from '@/components/ui'
+import { AdminNavigation, Button, Field, Layout, Loader, SlugWrapper } from '@/components/ui'
 import { useMovieEdit } from '@/components/screens/admin/movie/useMovieEdit'
 import { IMovieEditInput } from '@/shared/types/movie.interface'
 import { FC } from 'react'
 import { useForm } from 'react-hook-form'
-import {  View } from 'react-native'
+import {  ScrollView, View } from 'react-native'
+import { generateSlug } from '@/utils/generateSlug'
 
 const MovieEdit: FC = () => {
-	const {control, setValue, handleSubmit} = useForm<IMovieEditInput>({
+	const {control, setValue, handleSubmit, getValues} = useForm<IMovieEditInput>({
 		mode: 'onChange'
 	})
 
@@ -16,11 +17,33 @@ const MovieEdit: FC = () => {
 		<Layout isHasPadding>
 			<AdminNavigation title='Edit movie' isBackButton />
 			<View>
-				{isLoading ? <Loader/> : <>
-				<Button onPress={handleSubmit(onSubmit)} icon='pen-tool'>
-					Update
-				</Button>
-				</>}
+				{isLoading ? <Loader/> : <ScrollView showsVerticalScrollIndicator={false}>
+						<Field<IMovieEditInput>
+							control={control}
+							name='poster'
+							placeholder='Enter name'
+							rules={{
+								required: 'Name is required'
+							}}
+						/>
+						<SlugWrapper
+							generate={() => {
+								setValue('slug', generateSlug(getValues('title')))
+							}}
+						>
+							<Field<IMovieEditInput>
+								control={control}
+								name='slug'
+								placeholder='Enter slug'
+								rules={{
+									required: 'Slug is required'
+								}}
+							/>
+						</SlugWrapper>
+						<Button onPress={handleSubmit(onSubmit)} icon='pen-tool'>
+							Update
+						</Button>
+					</ScrollView>}
 			</View>
 		</Layout>
 	)
