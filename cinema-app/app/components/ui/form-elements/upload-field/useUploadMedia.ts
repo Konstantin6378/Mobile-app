4acf51f2dc507cb0d2db334c4840493186013952
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-05 14:06:34 +0700

    Update useUploadMedia.ts

--- cinema-app/app/components/ui/form-elements/upload-field/useUploadMedia.ts
+++ cinema-app/app/components/ui/form-elements/upload-field/useUploadMedia.ts
@@ -1,8 +1,9 @@
 type TypeUploadMedia = (onChange:(...value: any[]) => void, folder?: string) => {
-
+    uploadFile: () => Promise<void>
+    isLoading: boolean
 }
 
 
 export const useUploadMedia = ()  => {
-    
+
 }
