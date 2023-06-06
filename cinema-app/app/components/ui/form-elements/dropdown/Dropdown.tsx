c8ba6ca49c56b78095d5c2ef2b911b2bd3e3e91f
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-06-06 13:26:37 +0700

    Update Dropdown.tsx

---
+++ cinema-app/app/components/ui/form-elements/dropdown/Dropdown.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+import {View, Text} from 'react-native'
+
+const Dropdown: FC = () => {
+  return (
+      <View>
+         <Text>Dropdown</Text>
+      </View>
+  )
+}
+export default Dropdown
